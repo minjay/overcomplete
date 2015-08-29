330ec8bd09690b6fde0c6457d4c2b6f413d91512
minjay <mjfan@ucar.edu>
2015-08-29 12:30:12 -0600

    update

--- sim_MH2.m
+++ sim_MH2.m
@@ -62,14 +62,14 @@ tau0 = 0.01;
 tau0_sq_inv = 1/tau0^2;
 V0_inv = ones(M, 1); 
 eta0 = zeros(r, 1);
-alpha_sigma = 4;
-beta_sigma = 1;
+alpha_sigma = 0;
+beta_sigma = 0;
 tau_eta_sq = 0.25^2;
-sigma_eta_sq = 0.001;
-T = 250000;
+sigma_eta_sq = 0.0001;
+T = 150000;
 n_report = 100;
 burn_in = 50000;
-thin = 200;
+thin = 100;
 tic
 post_samples = Gibbs_sampler_MH2(A, Y, b_mat, fj_sq, nu, sigma0_sq,...
     tau0_sq_inv, V0_inv, eta0, alpha_sigma, beta_sigma, tau_eta_sq,...
