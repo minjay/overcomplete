0398a49ccfff2b4af19f0f4198a7e0f5638d55de
minjay <mjfan@ucar.edu>
2015-09-06 15:55:55 -0600

    sim_plot

---
+++ sim/plot_corr_fun.m
@@ -0,0 +1,21 @@
+function plot_corr_fun(B, j_min, j_max, nu, alpha, res, prop)
+
+l_max = floor(B^(j_max+1));
+sigma_j = B.^(-alpha*(j_min:j_max)/2);
+
+dist_vec = linspace(-1, 1, res)';
+Pl_mat = p_polynomial_value(res, l_max, dist_vec);
+cov_vec = zeros(res, 1);
+for j = j_min:j_max
+    l_min = ceil(B^(j-1));
+    l_max = floor(B^(j+1));
+    tmp = zeros(res, 1);
+    for l = l_min:l_max
+        tmp = tmp+(fun_b(l/B^j, B))^2*(2*l+1)/(4*pi)*Pl_mat(:, l+1);
+    end
+    cov_vec = cov_vec+nu*(sigma_j(j-j_min+1))^2/(nu-2)*tmp;
+end
+
+plot(acos(dist_vec), cov_vec/cov_vec(dist_vec==1), prop, 'LineWidth', 1.5)
+
+end
---
+++ sim/sim_plot.m
@@ -0,0 +1,19 @@
+subplot('position', [0.05 0.1 0.4 0.8])
+plot_corr_fun(2, 2, 4, 3, 2.1, 10000, 'b-')
+hold on
+plot_corr_fun(2, 2, 4, 3, 3, 10000, 'r--')
+plot_corr_fun(2, 2, 4, 3, 5, 10000, 'g-.')
+legend('\alpha=2.1', '\alpha=3', '\alpha=5')
+axis tight
+axis square
+title('(a)')
+
+subplot('position', [0.55 0.1 0.4 0.8])
+plot_corr_fun(2, 2, 4, 3, 3, 10000, 'b-')
+hold on
+plot_corr_fun(2, 3, 5, 3, 3, 10000, 'r--')
+plot_corr_fun(2, 4, 6, 3, 3, 10000, 'g-.')
+legend('j=2-4', 'j=3-5', 'j=4-6')
+axis tight
+axis square
+title('(b)')
