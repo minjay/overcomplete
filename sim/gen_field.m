fbf0a53b7dd74ae84f117a847eaceb9e0e6f236d
minjay <mjfan@ucar.edu>
2015-09-17 14:06:47 -0700

    modify gen_field

---
+++ sim/gen_field.m
@@ -0,0 +1,77 @@
+% parameter specification
+nu = 3;
+alpha = 4;
+sigma = 10;
+
+res = 200;
+
+B = 2;
+j_min = 2;
+j_max = 4;
+len_j = j_max-j_min+1;
+
+% grid points
+theta = linspace(0, pi, res/2);
+phi = linspace(0, 2*pi, res);
+
+[phi_mat, theta_mat] = meshgrid(phi, theta);
+theta_vec = theta_mat(:);
+phi_vec = phi_mat(:);
+
+% design matrix A
+[Npix, ~, A] = get_A_ss(B, j_min, j_max, theta_vec, phi_vec);
+[N, M] = size(A); 
+
+sigma_j = sigma*B.^(-alpha/2*(j_min:j_max));
+
+c = zeros(M, 1);
+f_j = cell(len_j, 1);
+st = 1;
+for j = j_min:j_max
+    index_j = j-j_min+1;
+    range = st:st+Npix(index_j)-1;
+    c(range) = sigma_j(index_j)*trnd(nu, Npix(index_j), 1);
+    f_j{index_j} = reshape(A(:, range)*c(range), res/2, res);
+    st = st+Npix(index_j);
+end
+
+plot(c)
+
+f = reshape(A*c, res/2, res);
+
+[L, T] = meshgrid(phi-pi, pi/2-theta);
+[HX, HY] = sph2hammer(L, T);
+
+% plot
+loc_x = [0 0.5 0 0.5];   
+loc_y = [0.5 0.5 0 0];
+
+figure
+for j = j_min:j_max
+    index_j = j-j_min+1;
+    subplot('position', [loc_x(index_j) loc_y(index_j) 0.45 0.45])
+    pcolor(HX, HY, f_j{index_j});
+    shading interp
+    axis equal
+    axis tight
+    axis off
+    colorbar('southoutside')
+    max_value = max(max(abs(f_j{index_j})));
+    caxis([-max_value, max_value])
+    title(['j = ', num2str(j)])
+end
+
+subplot('position', [loc_x(4) loc_y(4) 0.45 0.45])
+pcolor(HX, HY, f);
+shading interp
+axis equal
+axis tight
+axis off
+colorbar('southoutside')
+max_value = max(max(abs(f)));
+caxis([-max_value, max_value])
+title('Sum')
+
+% check non-Gaussianity
+figure
+qqplot(f(:))
