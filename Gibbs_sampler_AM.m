a7761d2f6924499809424dc7ab0fa183a3cf0308
minjay <mjfan@ucar.edu>
2015-09-22 14:40:14 -0700

    big update

--- Gibbs_sampler_AM.m
+++ Gibbs_sampler_AM.m
@@ -1,6 +1,54 @@
 function post_samples = Gibbs_sampler_AM(model, data, params, tuning, options)
+% GIBBS_SAMPLER_AM    The adaptive Metropolis-within-Gibbs sampler.
+%
+%   post_samples = Gibbs_sampler_AM(model, data, params, tuning, options);
+%
+% Inputs:
+%   model - the model
+%       model.A - the design matrix of the model, a N-by-M matrix
+%       model.fj_sq - f(j)^2, a M-by-1 vector, the index corresponds to
+%       psi_jk
+%       model.b_mat - the design matrix of the function b, a (r+1)-by-N
+%       matrix, each column corresponds to one location
+%       model.nu - the number of degrees of freedom
+%   data - the data
+%       data.Y - the observed values, a N-by-1 vector
+%       data.Npix - the number of spherical needlets at each frequency, a
+%       (j_max-j_min+1)-by-1 vector
+%   params - the initial values of the parameters
+%       params.c - the initial value of c, a M-by-1 vector
+%       params.V - the initial value of V^{-1}, a M-by-1 vector
+%       params.eta = {eta_init, tau_sigma_sq, tau_eta_sq}
+%           eta_init - the initial value of eta, a (r+1)-by-1 vector
+%           tau_sigma_sq - the parameter tau_sigma_sq
+%           tau_eta_sq - the parameter tau_eta_sq
+%       params.tau - the intial value of 1/tau^2
+%   tuning - the tuning parameters of the adaptive Metropolis
+%       tuning.mu - the initial value of mu
+%       (see Andrieu and Thoms, 2008, Algorithm 4)
+%       tuning.Sigma - the initial value of Sigma
+%       tuning.lambda - the initial value of lambda
+%   options - the MCMC run options
+%       options.T - the number of MCMC iterations
+%       options.burn-in - the length of the burn-in period
+%       options.thin - the length of the thinning interval
+%       options.n_report - the length of the interval to report progress
+% Outputs:
+%   post_samples - the posterior samples after discarding the samples in the
+%   burn-in period and thining
+%       post_samples.c - the posterior samples of c, a M-by-sample_size
+%       matrix
+%       post_samples.V_inv - the posterior samples of V^{-1}, a
+%       M-by-sample_size matrix
+%       post_samples.tau_sq_inv - the posterior samples of 1/tau^2, a
+%       1-by-sample_size vector
+%       post_samples.eta - the posterior samples of eta, a
+%       (r+1)-by-sample_size matrix
+%
+% Author: Minjie Fan, 2015
 
-time_st = cputime;
+tic
+
 % init
 A = model.A;
 fj_sq = model.fj_sq;
@@ -38,7 +86,7 @@ end
 r = length(eta)-1;
 
 % optimal acceptance rate
-% see Gelman, Roberts and Gilks (1996)
+% see Gelman et al., 1996
 rates = [0.441 0.352 0.316 0.279 0.275 0.266 0.261 0.255 0.261 0.267 0.234];
 if r+1<=10
     target_acc_rate = rates(r+1);
@@ -51,27 +99,22 @@ post_samples_c = zeros(M, sample_size);
 post_samples_V_inv = zeros(M, sample_size);
 post_samples_tau_sq_inv = zeros(1, sample_size);
 post_samples_eta = zeros(r+1, sample_size);
-flag = 0;
+
+DA = diag(exp(b_mat*eta))*A;
 acc_times = 0;
+
 for t = 1:T 
-    
-    if flag==0
-        HatA = diag(exp(b_mat'*eta))*A;
-    else
-        HatA = HatA_star;
-        flag = 0;
-    end
     
     % sample c
     for j = 1:len_j  
         z = randn(Npix(j), 1);
         range = st(j):en(j);
         not_range = [1:st(j)-1, en(j)+1:M];
-        HatA_j = HatA(:, range);
-        HatA_not_j = HatA(:, not_range);
-        Sigma_inv = tau_sq_inv*(HatA_j'*HatA_j)+diag(V_inv(range));
+        DA_j = DA(:, range);
+        DA_not_j = DA(:, not_range);
+        Sigma_inv = tau_sq_inv*(DA_j'*DA_j)+diag(V_inv(range));
         R = chol(Sigma_inv);
-        z = z+R'\(HatA_j'*Y-HatA_j'*HatA_not_j*c(not_range))*tau_sq_inv;
+        z = z+R'\(DA_j'*Y-DA_j'*DA_not_j*c(not_range))*tau_sq_inv;
         c(range) = R\z;
     end
    
@@ -82,24 +125,28 @@ for t = 1:T
     
     % sample tau
     shape = N/2;
-    HatAc = HatA*c;
-    quad_form = (Y-HatAc)'*(Y-HatAc);
+    DAc = DA*c;
+    quad_form = (Y-DAc)'*(Y-DAc);
     scale = 2/quad_form;
     tau_sq_inv = gamrnd(shape, scale);
     
     % sample eta
     eta_star = mvnrnd(eta, lambda*Sigma)';
     f1 = tau_sq_inv*quad_form/2+eta(2:r+1)'*eta(2:r+1)/2/tau_eta_sq+eta(1)^2/2/tau_sigma_sq;
-    Hat_star = diag(exp(b_mat'*eta_star));
-    HatA_star = Hat_star*A;
-    HatAc_star = HatA_star*c;
-    quad_form_star = (Y-HatAc_star)'*(Y-HatAc_star);
+    std_vec = exp(b_mat*eta_star);
+    DA_star = zeros(N, M);
+    for i = 1:N
+        DA_star(i, :) = std_vec(i)*A(i, :);
+    end
+    DAc_star = DA_star*c;
+    quad_form_star = (Y-DAc_star)'*(Y-DAc_star);
     f2 = tau_sq_inv*quad_form_star/2+eta_star(2:r+1)'*eta_star(2:r+1)/2/tau_eta_sq+eta_star(1)^2/2/tau_sigma_sq;
     ratio = exp(f1-f2);
     u = rand;
+    % accept the new sample of eta
     if ratio>=u
         eta = eta_star;
-        flag = 1;
+        DA = DA_star;
         acc_times = acc_times+1;
     end 
     
@@ -113,11 +160,15 @@ for t = 1:T
     
     % print to the screen
     if mod(t, n_report)==0
-        disp(['Sampled: ', num2str(t), ' of ', num2str(T)])
-        disp(['Metrop. Acceptance rate: ', num2str(floor(acc_times/n_report*1e4)/100), '%'])
+        if floor(t/n_report)==1
+            disp('--------------------------------------------')
+        end
+        fprintf('Sampled: %d of %d\n', t, T)
+        fprintf('Current Metropolis acceptance rate: %.2f%%\n',...
+        acc_times/n_report*100)
+        fprintf('Current lambda: %5f\n', lambda)
+        disp('--------------------------------------------')
         acc_times = 0;
-        lambda
-        Sigma
     end
     
     % save
@@ -135,5 +186,6 @@ end
 post_samples = struct('c', post_samples_c, 'V_inv', post_samples_V_inv,...
     'tau_sq_inv', post_samples_tau_sq_inv, 'eta', post_samples_eta);
 
-disp(num2str(cputime-time_st))
+toc
+
 end
