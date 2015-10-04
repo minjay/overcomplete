8b887c27fd3e20cb924eb090231bb61705ffdb8b
minjay <mjfan@ucar.edu>
2015-10-04 11:45:23 -0700

    use the data grid

--- real/real_A.m
+++ real/real_A.m
@@ -1,16 +1,15 @@
+% precompute the design matrix
+
+load('theta_phi.mat')
+
 B = 2;
 j_min = 2;
 j_max = 4;
 nu = 4;
 alpha = 4;
 
-res = 500;
-
-phi = linspace(0, 2*pi, res);
-theta = linspace(0, pi, res/2);
-[theta_mat, phi_mat] = meshgrid(theta, phi);
-theta_vec = theta_mat(:);
-phi_vec = phi_mat(:);
+theta_vec = theta(:);
+phi_vec = phi(:);
 
 % design matrix A
 [Npix, ~, A] = get_A_ss(B, j_min, j_max, theta_vec, phi_vec);
