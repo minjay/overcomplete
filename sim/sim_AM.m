bcb4f6a2349dab2de04c99b98705ed68a3d4a450
minjay <mjfan@ucar.edu>
2015-10-05 10:56:16 -0700

    save theta phi

--- sim/sim_AM.m
+++ sim/sim_AM.m
@@ -106,4 +106,4 @@ options = struct('T', T, 'burn_in', burn_in, 'thin', thin, 'n_report', n_report)
 
 post_samples = Gibbs_sampler_AM(model, data, params, tuning, options);
 
-save('post_samples.mat', 'post_samples', 'c', 'eta')
+save('post_samples.mat', 'post_samples', 'c', 'eta', 'theta', 'phi')
