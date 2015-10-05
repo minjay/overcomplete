c1364f1b48b8cb751ef30259cef992170ea4dd81
minjay <mjfan@ucar.edu>
2015-10-05 11:17:20 -0700

    delete grid

--- other/get_post_estimates.m
+++ other/get_post_estimates.m
@@ -1,27 +1,9 @@
 load('post_samples.mat')
 
-rng(1)
-
 % the grid
 B = 2;
-% change Nside to 8 or 16
-Nside = 8;
-tp = pix2ang(Nside, 'nest', false);
-N = length(tp);
-theta = zeros(N, 1);
-phi = zeros(N, 1);
-for i = 1:N
-    theta(i) = tp{i}(1);
-    phi(i) = tp{i}(2);
-end
 
-% perturbation
-theta = theta+randn(N, 1)*pi/10;
-theta(theta<0) = theta(theta<0)+pi;
-theta(theta>pi) = theta(theta>pi)-pi;
-phi = phi+randn(N, 1)*2*pi/10;
-phi(phi<0) = phi(phi<0)+2*pi;
-phi(phi>2*pi) = phi(phi>2*pi)-2*pi;
+N = length(theta);
 
 j_min = 2;
 j_max = 4;
