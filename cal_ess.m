a7cb9f6326f3651ab81a7313cedd20400f14ab8d
minjay <mjfan@ucar.edu>
2015-10-07 12:55:09 -0700

    update help comments

--- cal_ess.m
+++ cal_ess.m
@@ -1,6 +1,15 @@
 function N = cal_ess(post_samples)
 %CAL_ESS   Calculates the effective sample size based on the
 %autocorrelation function (ACF).
+%
+%   N = cal_ess(post_samples);
+%
+% Inputs:
+%   post_samples: posterior samples
+% Outputs:
+%   N: the effective sample size of the posterior samples
+%
+% Author: Minjie Fan, 2015
 
 acf = autocorr(post_samples);
 
