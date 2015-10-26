c241b480d412fcda50973eb860ffd87b341276ee
minjay <mjfan@ucar.edu>
2015-10-26 10:34:33 -0700

    new

---
+++ real/real_A_99.m
@@ -0,0 +1,19 @@
+% precompute the design matrix
+
+load('theta_phi.mat')
+
+B = 2;
+j_min = 2;
+j_max = 4;
+nu = 4;
+alpha = 4;
+
+theta_vec = theta(:);
+phi_vec = phi(:);
+
+% design matrix A
+[Npix, ~, A] = get_A_ss(B, j_min, j_max, theta_vec*4, phi_vec);
+
+save('mat_A.mat', 'A', '-v7.3')
+
+
