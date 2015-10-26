ae74dbfddadfaf51358e5bd268ce375db2ee627e
minjay <mjfan@ucar.edu>
2015-10-26 10:36:35 -0700

    update

--- real/real_A_99.m
+++ real/real_A_99.m
@@ -2,9 +2,9 @@
 
 load('theta_phi.mat')
 
-B = 2;
+B = 2.5;
 j_min = 2;
-j_max = 4;
+j_max = 3;
 nu = 4;
 alpha = 4;
 
@@ -14,6 +14,6 @@ phi_vec = phi(:);
 % design matrix A
 [Npix, ~, A] = get_A_ss(B, j_min, j_max, theta_vec*4, phi_vec);
 
-save('mat_A.mat', 'A', '-v7.3')
+save('mat_A_99.mat', 'A', '-v7.3')
 
 
