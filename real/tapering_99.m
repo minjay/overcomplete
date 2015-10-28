c3ebb1101882ad3122e77f69c53fe0888327822a
minjay <mjfan@ucar.edu>
2015-10-28 13:52:09 -0700

    new

---
+++ real/tapering_99.m
@@ -0,0 +1,34 @@
+% do tapering
+
+load('data.mat')
+
+resid_raw = resid;
+
+theta_vec = theta(:)/pi*180;
+n = length(theta_vec);
+
+% 25 degree co-latitude
+boundary = 25;
+
+% std parameter
+sigma_window = (max(theta_vec)-boundary)/0.5;
+
+% get weights
+index = find(theta_vec>boundary);
+weights = ones(n, 1);
+weights(index) = exp(-(theta_vec(index)-boundary).^2/2/sigma_window);
+
+% plot weights
+plot(theta_vec, weights, '.')
+
+% tapering
+resid = resid.*weights';
+
+% plot
+plot_pot_lite(reshape(resid, size(phi)), phi, theta, 1000, max(abs(resid)))
+
+diff = resid_raw-resid;
+plot_pot(reshape(diff, size(phi)), phi, theta, 1000)
+
+% save
+save('data_taper.mat', 'resid', 'theta', 'phi')
