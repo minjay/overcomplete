e46de2080fa9c05ffc70e873192f23fbc66331fa
minjay <mjfan@ucar.edu>
2015-11-02 11:59:24 -0800

    add curly brackets

--- sim/sim_plot.m
+++ sim/sim_plot.m
@@ -3,7 +3,7 @@ plot_corr_fun(2, 2, 4, 3, 2.1, 10000, 'b-')
 hold on
 plot_corr_fun(2, 2, 4, 3, 3, 10000, 'r--')
 plot_corr_fun(2, 2, 4, 3, 5, 10000, 'g-.')
-legend('\alpha=2.1', '\alpha=3', '\alpha=5', 'FontSize', 15)
+legend({'\alpha=2.1', '\alpha=3', '\alpha=5'}, 'FontSize', 15)
 axis tight
 axis square
 title('(a)', 'FontSize', 15)
@@ -13,7 +13,7 @@ plot_corr_fun(2, 2, 4, 3, 3, 10000, 'b-')
 hold on
 plot_corr_fun(2, 3, 5, 3, 3, 10000, 'r--')
 plot_corr_fun(2, 4, 6, 3, 3, 10000, 'g-.')
-legend('j=2-4', 'j=3-5', 'j=4-6', 'FontSize', 15)
+legend({'j=2-4', 'j=3-5', 'j=4-6'}, 'FontSize', 15)
 axis tight
 axis square
 title('(b)', 'FontSize', 15)
