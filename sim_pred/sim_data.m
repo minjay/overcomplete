4322c9c12ba909a729cd5823952554c606c871de
minjay <mjfan@ucar.edu>
2016-02-02 11:31:30 -0800

    update

--- sim_pred/sim_data.m
+++ sim_pred/sim_data.m
@@ -2,7 +2,7 @@ function sim_data(nu, name)
 
 rng(1)
 
-alpha = 4;
+alpha = 3;
 
 tau = 0.1;
 
@@ -18,7 +18,7 @@ phi_vec = phi_mat(:);
 
 B = 2;
 j_min = 2;
-j_max = 4;
+j_max = 3;
 
 % design matrix A
 [Npix, ~, A] = get_A_ss(B, j_min, j_max, theta_vec, phi_vec);
@@ -39,11 +39,13 @@ for i = 1:N
     DA(i, :) = std_vec(i)*A(i, :);
 end
 
+fj_sq = zeros(M, 1);
 c = zeros(M, 1);
 st = 1;
 for j = j_min:j_max
     index_j = j-j_min+1;
     range = st:st+Npix(index_j)-1;
+    fj_sq(range) = B^(-alpha*j)*ones(Npix(index_j), 1);
     c(range) = sigma_j(index_j)*trnd(nu, Npix(index_j), 1);
     st = st+Npix(index_j);
 end
@@ -51,6 +53,6 @@ end
 Y = DA*c+tau*randn(N, 1);
 
 save(['data_sim_', name, '.mat'], 'theta', 'phi', 'theta_mat', 'phi_mat', 'theta_vec',...
-    'phi_vec', 'Y', 'nu', 'alpha', 'tau', 'sigma_j', 'eta')
+    'phi_vec', 'Y', 'nu', 'alpha', 'tau', 'sigma_j', 'eta', 'fj_sq')
 
 end
