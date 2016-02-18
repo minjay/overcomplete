24e550239a56564a40dd5c286a279ca0acec10a7
minjay <mjfan@ucar.edu>
2016-02-18 15:05:54 -0800

    update

--- sim_pred/sim_plot/plot_post_dist.m
+++ sim_pred/sim_plot/plot_post_dist.m
@@ -34,6 +34,22 @@ hold on
 ff = normpdf(xi, mean_Matern, std_Matern);
 plot(xi, ff, 'r', 'LineWidth', 1.5)
 
+loc3 = 2010;
+[f, xi] = ksdensity(Y_pred_all(index_pred(loc3), :));
+figure
+plot(xi, f, 'LineWidth', 1.5)
+hold on
+ff = normpdf(xi, Y_pred_Matern(index_pred(loc3)), std_Y_pred_Matern(loc3));
+plot(xi, ff, 'r', 'LineWidth', 1.5)
+
+loc4 = 2330;
+[f, xi] = ksdensity(Y_pred_all(index_pred(loc4), :));
+figure
+plot(xi, f, 'LineWidth', 1.5)
+hold on
+ff = normpdf(xi, Y_pred_Matern(index_pred(loc4)), std_Y_pred_Matern(loc4));
+plot(xi, ff, 'r', 'LineWidth', 1.5)
+
 figure
 scatter(phi_samples, theta_samples, [], Y_samples, '.')
 axis equal
@@ -41,4 +57,7 @@ axis tight
 colorbar
 caxis([-max(abs(Y_samples)) max(abs(Y_samples))])
 hold on
-plot(phi_vec(index_pred([loc1 loc2])), theta_vec(index_pred([loc1 loc2])), 'ko')
+text(phi_vec(index_pred(loc1)), theta_vec(index_pred(loc1)), '1')
+text(phi_vec(index_pred(loc2)), theta_vec(index_pred(loc2)), '2')
+text(phi_vec(index_pred(loc3)), theta_vec(index_pred(loc3)), '3')
+text(phi_vec(index_pred(loc4)), theta_vec(index_pred(loc4)), '4')
