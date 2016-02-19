18c0332e0cb9a37a9d98658451542b41ae72adda
minjay <mjfan@ucar.edu>
2016-02-19 14:46:29 -0800

    rename and redefine its functionality

---
+++ other/plot_samples.m
@@ -0,0 +1,24 @@
+function plot_samples(theta_vec, index, phi_samples, pot_samples)
+
+theta_vec = (pi/2-theta_vec)/pi*180;
+theta_min = min(theta_vec);
+lonlim = [0 360];
+latlim = [theta_min 90];
+
+worldmap(latlim, lonlim);
+lats = theta_vec(index);
+lons = phi_samples/pi*180;
+scatterm(lats, lons, [], pot_samples, 'filled');
+colorbar
+max_caxis = max(abs(pot_samples));
+caxis([-max_caxis max_caxis])
+
+setm(gca, 'ParallelLabel', 'off', 'MeridianLabel', 'off')
+textm(75, 300, '15\circ');
+textm(60, 300, '30\circ');
+textm(50, 177.5, '00')
+textm(52.5, 270, '06')
+textm(50, 2.5, '12')
+textm(47.5, 90, '18')
+
+end
