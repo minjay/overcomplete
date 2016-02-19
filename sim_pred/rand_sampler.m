7f547a81fde39b61ed9b510713b934ea089f0ece
minjay <mjfan@ucar.edu>
2016-02-19 14:17:19 -0800

    update sampling scheme

--- sim_pred/rand_sampler.m
+++ sim_pred/rand_sampler.m
@@ -1,7 +1,10 @@
 function index = rand_sampler(phi_vec)
 
-index = find(phi_vec<=pi-pi/18 | phi_vec>=pi+pi/18);
+index = find((phi_vec<=pi-pi/18 | phi_vec>=pi+pi/18) & theta_vec>=pi/6 & theta_vec<=5*pi/6);
 n = 1e3;
-index = randsample(index, n);
+index1 = randsample(index, n*0.9);
+index = find((phi_vec<=pi-pi/18 | phi_vec>=pi+pi/18) & (theta_vec<pi/6 | theta_vec>5*pi/6));
+index2 = randsample(index, n*0.1);
+index = [index1; index2];
 
 end
