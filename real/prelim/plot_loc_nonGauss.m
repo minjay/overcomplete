6ba8f7fba3ea83048c608464ffaccc967be43d85
minjay <mjfan@ucar.edu>
2016-02-23 15:28:47 -0800

    add qqplot

--- real/prelim/plot_loc_nonGauss.m
+++ real/prelim/plot_loc_nonGauss.m
@@ -1,4 +1,4 @@
-load('resid_regr_all.mat')
+load('data_EOF_regr.mat')
 
 phi = phi/pi*180;
 theta = (pi/2-theta)/pi*180;
@@ -12,8 +12,11 @@ latlim = [theta_min 90];
 
 index = 1500*(1:9);
 worldmap(latlim, lonlim)
-scatterm(theta_vec(index), phi_vec(index), 'o', 'filled') 
+for i = 1:9
+    textm(theta_vec(index(i)), phi_vec(index(i)), num2str(i))
+end
 
+figure
 for i = 1:9
     subplot(3, 3, i)
     [f, xi] = ksdensity(resid_all(:, index(i)));
@@ -22,4 +25,11 @@ for i = 1:9
     ff = normpdf(xi, mean(resid_all(:, index(i))), std(resid_all(:, index(i))));
     plot(xi, ff, 'r')
 end
+
+figure
+for i = 1:9
+    subplot(3, 3, i)
+    qqplot(resid_all(:, index(i)));
+end
+
     
