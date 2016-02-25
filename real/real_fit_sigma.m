605624d85a07c83c8d39592bc3fdb4a252112fc9
minjay <mjfan@ucar.edu>
2016-02-24 20:28:25 -0800

    correct one mistake

--- real/real_fit_sigma.m
+++ real/real_fit_sigma.m
@@ -20,7 +20,7 @@ pot_samples = resid(index)';
 % parameter specification
 B = 2;
 j_min = 2;
-j_max = 3;
+j_max = 4;
 nu = 3;
 
 % design matrix A
@@ -45,9 +45,9 @@ sigma_j_sq_init = ones(j_max-j_min, 1);
 % eta
 eta_init = zeros(r+1, 1);
 % pri_sig of eta_0
-tau_sigma_sq = 0.25^2;
+tau_sigma_sq = 1e4;
 % pri_sig of eta
-tau_eta_sq = 1e4;
+tau_eta_sq = 0.25^2;
 % tau
 tau_init = 0.01;
 tau_sq_inv_init = 1/tau_init^2;
