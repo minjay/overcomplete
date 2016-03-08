28d59db0467334f533a7787b26cca0f75bf84771
minjay <mjfan@ucar.edu>
2016-03-08 13:30:11 -0800

    3+2=5

--- real/real_fit_sigma_test_B_spline.m
+++ real/real_fit_sigma_test_B_spline.m
@@ -41,8 +41,8 @@ nu = 3;
 M = size(A, 2);
 
 % non-stationary variance function
-knots = [0 0 0 0 0.25 0.5 0.75 1 1 1 1]*pi;
-[b_mat, ~] = bspline_basismatrix(4, knots, theta_samples*4);
+knots = [0 0 0 1/3 2/3 1 1 1]*pi;
+[b_mat, ~] = bspline_basismatrix(3, knots, theta_samples*4);
 
 r = size(b_mat, 2);
 
