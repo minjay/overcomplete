ffd5acc1b58c035ab5e2633fc6ebd341c592daf9
minjay <mjfan@ucar.edu>
2016-03-14 17:05:01 -0700

    update

--- real/real_fit_sigma_test_const.m
+++ real/real_fit_sigma_test_const.m
@@ -1,5 +1,7 @@
 load('data_EOF_regr.mat')
 resid = resid_all(1, :);
+emp_std_vec = std(resid_all, 0, 1);
+resid = resid./emp_std_vec;
 
 rng(1)
 
@@ -25,12 +27,6 @@ nu = 3;
 [Npix, ~, A] = get_A_ss(B, j_min, j_max, theta_samples*4, phi_samples);
 M = size(A, 2);
 
-% non-stationary variance function
-knots = [0 0 0 0 0.3 1 1 1 1]*pi;
-[b_mat, ~] = bspline_basismatrix(4, knots, theta_samples*4);
-
-r = size(b_mat, 2);
-
 % rescale the observations
 Y = pot_samples/1e3;
 
@@ -42,24 +38,16 @@ c_init = zeros(M, TT);
 % V
 V_inv_init = ones(M, 1); 
 % sigma_j_sq
-sigma_j_sq_init = ones(j_max-j_min, 1);
-% eta
-eta_init = zeros(r, 1);
-% pri_sig of eta
-tau_eta_sq = 1e4;
+sigma_j_sq_init = ones(j_max-j_min+1, 1);
 % tau
 tau_init = 0.01;
 tau_sq_inv_init = 1/tau_init^2;
-% tuning parameters
-mu_init = zeros(r, 1);
-Sigma_init = eye(r);
-lambda = 0.001;
 % the number of MCMC iterations
-T = 3e5;
+T = 1e5;
 % the length of the burn-in period
 burn_in = 0;
 % the length of the thinning interval
-thin = 250;
+thin = 100;
 % the length of the interval to report progress
 n_report = 100;
 
@@ -68,12 +56,10 @@ model = struct('A', A, 'b_mat', b_mat, 'nu', nu);
 data = struct('Y', Y, 'Npix', Npix);
 
 params = struct('c', c_init, 'V', V_inv_init, 'sigma_j_sq', sigma_j_sq_init,...
-    'eta', eta_init, 'tau_eta_sq', tau_eta_sq, 'tau', tau_sq_inv_init);
-
-tuning = struct('mu', mu_init, 'Sigma', Sigma_init, 'lambda', lambda);
+    'tau', tau_sq_inv_init);
 
 options = struct('T', T, 'burn_in', burn_in, 'thin', thin, 'n_report', n_report);
 
-post_samples = Gibbs_sampler_AM4(model, data, params, tuning, options);
+post_samples = Gibbs_sampler_AM10(model, data, params, options);
 
 save('post_samples_real.mat', 'post_samples', 'Npix', 'index', 'theta_samples', 'phi_samples')
