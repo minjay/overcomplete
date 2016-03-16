1d2400ffbe975e192e4b06ea6d619852ef229443
minjay <mjfan@ucar.edu>
2016-03-15 21:50:40 -0700

    1500

--- real/rand_sampler_real.m
+++ real/rand_sampler_real.m
@@ -1,7 +1,7 @@
 function index = rand_sampler_real(theta_vec)
 
 index = find(theta_vec>=pi/6 & theta_vec<=5*pi/6);
-n = 1000;
+n = 1500;
 index1 = randsample(index, n*0.9);
 index = find(theta_vec<pi/6 | theta_vec>5*pi/6);
 index2 = randsample(index, n*0.1);
