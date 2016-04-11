b8ca100a5e66e1f34ddc68ad875a02c73f503fdd
minjay <mjfan@ucar.edu>
2016-04-10 22:03:06 -0700

    get more data points

--- real/prelim/get_real_data_regr.m
+++ real/prelim/get_real_data_regr.m
@@ -5,19 +5,19 @@ load('svd.mat')
 load('theta_phi.mat')
 
 r = double(r);
-r = r(:, 1:16920);
-phi = phi(:, 1:end-1);
-theta = theta(:, 1:end-1);
-
-emp_std_vec = std(r, 0, 1);
+% remove the obs when theta=0
+r = r(:, 361:end);
+phi = phi(:, 2:end);
+theta = theta(:, 2:end);
 
 T = size(r, 1);
 
 L = 3;
 M = 3;
 
-resid_all = zeros(size(r));
-for t = 1:100 
+times = 1:10:T;
+resid_all = zeros(times, size(r, 2));
+for t = times
     [coef, resid, y_hat, ~] = SCHA_regr(reshape(r(t, :)./emp_std_vec, size(phi)), theta, phi, L, M);
     resid_all(t, :) = resid.*emp_std_vec';
 end
@@ -25,5 +25,4 @@ end
 index = 1;
 plot_pot(reshape(resid_all(index, :), size(phi)), phi, theta, 1000, max(abs(resid_all(index, :))));
 
-resid_all=resid_all(1:100,:);
 save('data_EOF_regr_new.mat', 'resid_all', 'theta', 'phi', '-v7.3')
