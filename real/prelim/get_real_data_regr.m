4274ffcbadaf1bb394670e4e333e102a8846bad8
minjay <mjfan@ucar.edu>
2016-04-13 12:54:06 -0700

    update

--- real/prelim/get_real_data_regr.m
+++ real/prelim/get_real_data_regr.m
@@ -15,17 +15,19 @@ T = size(r, 1);
 
 L = 3;
 M = 3;
-
-emp_std_vec = std(r, 0, 1);
+multi = 4;
 
 times = 1:10:T;
+emp_std_vec = std(r(times, :), 0, 1);
+
 resid_all = zeros(length(times), size(r, 2));
 for t = 1:length(times)
-    [coef, resid, y_hat, ~] = SCHA_regr(reshape(r(times(t), :)./emp_std_vec, size(phi)), theta, phi, L, M);
+    [coef, resid, y_hat, ~] = SCHA_regr(reshape(r(times(t), :)./emp_std_vec, size(phi)), theta, phi, L, M, multi);
     resid_all(t, :) = resid'.*emp_std_vec;
 end
 
 index = 1;
+% to do: revise plot_pot
 plot_pot(reshape(resid_all(index, :), size(phi)), phi, theta, 1000, max(abs(resid_all(index, :))));
 
 save('data_EOF_regr_new.mat', 'resid_all', 'theta', 'phi', '-v7.3')
