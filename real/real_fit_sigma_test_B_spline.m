310025d01366b345cc3e5dfdd5227bd96bb3e2a9
minjay <mjfan@ucar.edu>
2016-04-14 23:03:39 -0700

    num of iter=3e5

--- real/real_fit_sigma_test_B_spline.m
+++ real/real_fit_sigma_test_B_spline.m
@@ -56,7 +56,7 @@ mu_init = zeros(r, 1);
 Sigma_init = eye(r);
 lambda = 0.001;
 % the number of MCMC iterations
-T = 6e5;
+T = 3e5;
 % the length of the burn-in period
 burn_in = 0;
 % the length of the thinning interval
