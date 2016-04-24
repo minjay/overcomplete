45048fea82178d7837fee7c7198943b385c06851
minjay <mjfan@ucar.edu>
2016-04-24 11:24:26 -0700

    only save the first 10 c's

--- real/real_fit_sigma_test_SH_prior.m
+++ real/real_fit_sigma_test_SH_prior.m
@@ -80,4 +80,6 @@ options = struct('T', T, 'burn_in', burn_in, 'thin', thin, 'n_report', n_report)
 
 post_samples = Gibbs_sampler_AM10(model, data, params, tuning, options);
 
+post_samples.c = post_samples.c(:, 1:10, :);
+
 save('post_samples_real.mat', 'post_samples', 'Npix', 'index', 'theta_samples', 'phi_samples', '-v7.3')
