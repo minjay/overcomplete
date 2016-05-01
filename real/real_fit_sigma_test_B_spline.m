a73b2e556987f7684bebdb4dfa85a01dd60163df
minjay <mjfan@ucar.edu>
2016-04-30 22:52:21 -0700

    strong prior

--- real/real_fit_sigma_test_B_spline.m
+++ real/real_fit_sigma_test_B_spline.m
@@ -52,8 +52,8 @@ tau_eta_sq = 1e2;
 tau_init = 0.01;
 tau_sq_inv_init = 1/tau_init^2;
 % gamma prior
-alpha_sigma = 2*0.1;
-beta_sigma = 2;
+alpha_sigma = 10*0.1;
+beta_sigma = 10;
 % tuning parameters
 mu_init = zeros(r, 1);
 Sigma_init = eye(r);
