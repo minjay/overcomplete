79f27d184c2d7696b682d737bc1267c5b7e4f553
minjay <mjfan@ucar.edu>
2016-05-01 22:34:17 -0700

    0.005

--- real/real_fit_sigma_test_B_spline_no_sigma.m
+++ real/real_fit_sigma_test_B_spline_no_sigma.m
@@ -62,7 +62,7 @@ for j = 1:len_j
     en(j) = sum(Npix(1:j));
 end
 fj_sq = zeros(M, 1);
-sigma_j_sq = [1 0.02];
+sigma_j_sq = [1 0.005];
 for j = 1:len_j
     range = st(j):en(j);
     fj_sq(range) = sigma_j_sq(j)*ones(Npix(j), 1);
