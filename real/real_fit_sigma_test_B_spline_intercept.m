3145d747a7c51251323e60e992a92a44b10e0629
minjay <mjfan@ucar.edu>
2016-05-04 23:55:25 -0700

    update

--- real/real_fit_sigma_test_B_spline_intercept.m
+++ real/real_fit_sigma_test_B_spline_intercept.m
@@ -7,7 +7,7 @@ rng(1)
 theta_vec = theta(:);
 phi_vec = phi(:);
 w = sin(theta_vec*4);
-[pot_samples, index] = datasample(resid', 2000, 'Replace', false,...
+[pot_samples, index] = datasample(resid', 4000, 'Replace', false,...
     'Weights', w);
 theta_samples = theta_vec(index);
 phi_samples = phi_vec(index);
@@ -19,7 +19,7 @@ phi_samples = phi_vec(index);
 % parameter specification
 B = 2;
 j_min = 2;
-j_max = 3;
+j_max = 4;
 nu = 4;
 
 % design matrix A
@@ -46,7 +46,7 @@ c_init = zeros(M, TT);
 % V
 V_inv_init = ones(M, 1); 
 % sigma_j_sq
-sigma_j_sq_init = ones(j_max-j_min, 1);
+sigma_j_sq_init = [0.01; 0.0002];
 % eta
 eta_init = zeros(r+1, 1);
 % pri_sig of eta_0
@@ -56,15 +56,12 @@ tau_eta_sq = 0.25^2;
 % tau
 tau_init = 0.01;
 tau_sq_inv_init = 1/tau_init^2;
-% gamma prior
-alpha_sigma = 5*0.1;
-beta_sigma = 5;
 % tuning parameters
 mu_init = zeros(r+1, 1);
 Sigma_init = eye(r+1);
 lambda = 0.001;
 % the number of MCMC iterations
-T = 4e5;
+T = 6e5;
 % the length of the burn-in period
 burn_in = 0;
 % the length of the thinning interval
@@ -78,12 +75,12 @@ data = struct('Y', Y, 'Npix', Npix);
 
 params = struct('c', c_init, 'V', V_inv_init, 'sigma_j_sq', sigma_j_sq_init,...
     'eta', eta_init, 'tau_sigma_sq', tau_sigma_sq, 'tau_eta_sq', tau_eta_sq,...
-    'tau', tau_sq_inv_init, 'alpha_sigma', alpha_sigma, 'beta_sigma', beta_sigma);
+    'tau', tau_sq_inv_init);
 
 tuning = struct('mu', mu_init, 'Sigma', Sigma_init, 'lambda', lambda);
 
 options = struct('T', T, 'burn_in', burn_in, 'thin', thin, 'n_report', n_report);
 
-post_samples = Gibbs_sampler_AM_Gamma_prior(model, data, params, tuning, options);
+post_samples = Gibbs_sampler_AM_rep_inter(model, data, params, tuning, options);
 
 save('post_samples_real.mat', 'post_samples', 'Npix', 'index', 'theta_samples', 'phi_samples')
