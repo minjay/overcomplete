7fd298770d7b4791ce467b8a7781f0ebc2cef41b
minjay <mjfan@ucar.edu>
2016-05-07 15:47:19 -0700

    update to no_sigma

--- real/post_assess_no_sigma.m
+++ real/post_assess_no_sigma.m
@@ -3,23 +3,11 @@ load('data_EOF_regr_new.mat')
 resid = resid_all(1, :);
 
 for i = 1:6
-    subplot(3, 3, i)
+    subplot(3, 2, i)
     plot(post_samples.eta(i, :))
     axis tight
     title(['eta', num2str(i)])
 end
-subplot(3, 3, 7)
-plot(post_samples.sigma_j_sq(2, :))
-axis tight
-title('sigmajsq2')
-subplot(3, 3, 8)
-plot(post_samples.sigma_j_sq(3, :))
-axis tight
-title('sigmajsq3')
-subplot(3, 3, 9)
-plot(post_samples.tau_sq_inv)
-axis tight
-title('tausqinv')
 
 load('mat_A.mat')
 [N, M] = size(A);
@@ -31,6 +19,7 @@ theta_vec = theta(:);
 % non-stationary variance function
 knots = [0 0 0 0 40/180 80/180 1 1 1 1]*pi;
 [b_mat, ~] = bspline_basismatrix(4, knots, theta_vec*4);
+b_mat(:, 1) = 1;
 
 st = 1;
 en = size(post_samples.eta, 2);
@@ -51,14 +40,18 @@ figure
 semilogy(pred_err)
 
 figure
+Y_err = resid'-DAc(:, en-st+1);
+plot_pot_with_obs(reshape(Y_err, size(phi)), phi, theta, phi_samples, theta_samples, 1000)
+
+figure
 hold on
 std_vec = exp(b_mat*post_samples.eta);
 % plot fitted std function
 nu = 4;
 for i = 1:100:size(post_samples.eta, 2)
-    sigma_j = sqrt(post_samples.sigma_j_sq(:, i));
+    sigma_j = sqrt([1 0.01 0.0002]);
     variance = plot_corr_fun_flex(2, sigma_j, 2, 4, nu, 1000, 'r');
     std_vec(:, i) = std_vec(:, i)*sqrt(variance)*1000;
 end
 figure
-plot(theta_vec, std_vec(:, 1:100:end))
+plot(theta_vec, std_vec(:, 1001:100:end))
