0c1900ffba0db9894592ff971988b39a52a093bd
minjay <mjfan@ucar.edu>
2016-05-08 21:23:50 -0700

    revise

--- real/simulate/uncond_sim.m
+++ real/simulate/uncond_sim.m
@@ -18,13 +18,14 @@ theta_vec = theta(:);
 
 % simulate
 % non-stationary variance function
-knots = [0 0 0 0 40/180 80/180 120/180 1 1 1 1]*pi;
+knots = [0 0 0 0 40/180 80/180 1 1 1 1]*pi;
 [b_mat, ~] = bspline_basismatrix(4, knots, theta_vec*4);
+b_mat(:, 1) = 1;
 
 % discard burn-in period
 burn_in = 500;
 post_samples_eta = post_samples.eta(:, burn_in+1:end);
-post_samples_sigma_j = sqrt(post_samples.sigma_j_sq(:, burn_in+1:end));
+post_samples_sigma_j = sqrt([1; 0.01; 0.0002]);
 post_samples_tau = 1./sqrt(post_samples.tau_sq_inv(burn_in+1:end));
 
 % num of posterior samples
@@ -38,10 +39,13 @@ std_vec = exp(b_mat*post_samples_eta);
 A = A(361:(N-360), :);
 [N, M] = size(A);
 
-Y = zeros(N, n_sample);
+T = 9;
+Y = zeros(N, T);
+rng(1)
 
-for i = 1:n_sample
-    i
+for t = 1:T
+    
+    i = n_sample;
     DA = zeros(N, M);
     for j = 1:N
         DA(j, :) = std_vec(j, i)*A(j, :);
@@ -51,14 +55,14 @@ for i = 1:n_sample
     for j = j_min:j_max
         index_j = j-j_min+1;
         range = st:st+Npix(index_j)-1;
-        c(range) = post_samples_sigma_j(index_j, i)*trnd(nu, Npix(index_j), 1);
+        c(range) = post_samples_sigma_j(index_j)*trnd(nu, Npix(index_j), 1);
         st = st+Npix(index_j);
     end
-    Y(:, i) = DA*c+post_samples_tau(i)*randn(N, 1);
-end
+    Y(:, t) = (DA*c+post_samples_tau(i)*randn(N, 1))*1e3;
+    subplot(3, 3, t)
+    plot_pot(reshape(Y(:, t), size(phi)), phi, theta, 1000, max(abs(Y(:, t))))
 
-% rescale
-Y = Y*1e3;
+end
 
 % save
 save('uncond_sim.mat', 'Y')
