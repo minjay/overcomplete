916e5d7f72b7926c2814fd5bcf080bacd56fef33
minjay <mjfan@ucar.edu>
2016-06-07 11:27:38 -0700

    save electric fields

--- real/simulate/get_large_scale_energy.m
+++ real/simulate/get_large_scale_energy.m
@@ -14,20 +14,7 @@ HX = theta(1, :);
 HY = phi(:, 1);
 [FX, FY] = gradient(large_scale, HX, HY);
 R = 6.5*1e6;
-FX = -FX/R;
-FY = -FY./(R*sin(theta));
+E_theta_large_scale = -FX/R;
+E_phi_large_scale = -FY./(R*sin(theta));
 
-figure
-plot_pot(FX, phi, theta, 1000, max(abs(FX(:))))
-figure
-plot_pot(FY, phi, theta, 1000, max(abs(FY(:))))
-
-energy_large_scale = FX.^2+FY.^2;
-
-load('WHI_quad_cond.mat')
-P_cond = all_Cond_N{1}(:);
-
-P_cond = P_cond(361:(end-360));
-energy_large_scale = energy_large_scale(:).*P_cond;
-
-save('energy_large_scale.mat', 'energy_large_scale')
+save('energy_large_scale.mat', 'E_theta_large_scale', 'E_phi_large_scale')
