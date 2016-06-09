89fd6f80816bd0ab403880911dfa0af7ad0346b4
minjay <mjfan@ucar.edu>
2016-06-09 16:43:26 -0600

    add script to plot std fun (all models)

--- real/plot/plot_std_fun_CI.m
+++ real/plot/plot_std_fun_CI.m
@@ -1,37 +1,74 @@
-load('theta_phi.mat')
-load('post_samples_real.mat')
+clear
 
-theta_vec = theta(:);
-phi_vec = phi(:);
+% load data
+load('data_EOF_regr_new.mat')
+load('post_samples_real_new_resid_j24_pen_B_spline_cubic_2knots_aurora_n4000_sigma_j_sq_0.01_0.0002_nu4_long_init0.mat')
+
+% get theta and phi
+theta_vec = theta(1, :)';
+phi_vec = phi(1, :)';
 
 N = length(theta_vec);
 
-eta_est = mean(post_samples.eta, 2);
+post_samples_eta = post_samples.eta(:, 1501:end);
 
-r = 4;
-mu = pi/(r+1)*(1:r);
-lambda = pi/(r+1)*2.5/2;
-b_mat = zeros(N, r+1);
+eta_est = mean(post_samples_eta, 2);
+
+% non-stationary variance function
+knots = [0 0 0 0 40/180 80/180 1 1 1 1]*pi;
+[b_mat, ~] = bspline_basismatrix(4, knots, theta_vec*4);
+
 b_mat(:, 1) = 1;
-for i = 2:r+1
-    b_mat(:, i) = exp(-(theta_vec*4-mu(i-1)).^2/2/lambda^2);
-end
 
 std_vec_est = exp(b_mat*eta_est);
 
-n_samples = size(post_samples.eta, 2);
-std_vec_post = zeros(N, n_samples);
-for i = 1:n_samples
-    std_vec_post(:, i) = exp(b_mat*post_samples.eta(:, i));
-end
+std_vec_post = exp(b_mat*post_samples_eta);
+
+nu = 4;
+sigma_j = sqrt([1 0.01 0.0002]);
+variance = plot_corr_fun_flex(2, sigma_j, 2, 4, nu, 1000, 'r');
+std_vec_est = std_vec_est*sqrt(variance);
+std_vec_post = std_vec_post*sqrt(variance);
 
 CI = quantile(std_vec_post', [0.025 0.975]);
 
-[theta_sort, index] = sort(theta_vec);
-lat_sort = theta_sort/pi*180;
-plot(lat_sort, std_vec_est(index), 'b-', 'LineWidth', 1.5)
+% plot empirical std function
+figure
+emp_std_vec = std(resid_all, 0, 1);
+h1 = plot(theta(:)/pi*180, emp_std_vec/1e3, '.');
+
 hold on
-plot(lat_sort, CI(1, index), 'r--', 'LineWidth', 1.5)
-plot(lat_sort, CI(2, index), 'r--', 'LineWidth', 1.5)
-legend('Est. std function', '95% CI endpoints')
+
+%%% plot fitted std function by nonGau-need
+h2 = plot(theta_vec/pi*180, std_vec_est, 'b-', 'LineWidth', 2);
+hold on
+h3 = plot(theta_vec/pi*180, CI(1, :), 'r--', 'LineWidth', 2);
+plot(theta_vec/pi*180, CI(2, :), 'r--', 'LineWidth', 2)
+
+%%% plot fitted std function by Gau-need
+load('beta_hat_needlet.mat')
+beta = beta_hat(1:end-1);
+B = 2;
+j_min = 2;
+j_max = 4;
+[Npix, ~, A] = get_A_ss(B, j_min, j_max, theta_vec*4, phi_vec);
+cov_mat = get_cov_Gaussian_needlet(beta, b_mat, Npix, A);
+h4 = plot(theta_vec/pi*180, sqrt(diag(cov_mat)), 'LineWidth', 2);
+
+%%% plot fitted std function by Matern
+load('beta_hat.mat')
+beta = beta_hat(1:end-1);
+% stretching
+[x, y, z] = trans_coord(theta_vec*4, phi_vec);
+% get distance matrix
+r = get_chordal_dist(x, y, z);
+cov_mat = get_cov_nonsta_Matern(beta, r, b_mat);
+h5 = plot(theta_vec/pi*180, sqrt(diag(cov_mat)), 'LineWidth', 2);
+
+lg = legend([h1 h2 h3 h4 h5], {'Empirical std function', 'Est. std function (nonGau-need)',...
+    '95% CI endpoints', 'Est. std function (Gau-need)', 'Est. std function (Gau-Matern)'});
+set(lg, 'FontSize', 12);
+xlabel('Co-latitude (\circ)')
+ylabel('Standard deviation (kV)')
+set(gca, 'FontSize', 12);
 axis tight
