380d2496f50d7bae07d96eff25b1b45a308acef5
minjay <mjfan@ucar.edu>
2016-06-10 09:46:56 -0600

    plot more fitted corr funs

--- real/plot/plot_corr_fun.m
+++ real/plot/plot_corr_fun.m
@@ -1,3 +1,5 @@
+clear
+
 load('data_EOF_regr_new.mat')
 r = resid_all;
 
@@ -30,5 +32,42 @@ end
 
 plot(dist_vec, corr_vec, '.')
 hold on
-plot_corr_fun(2, 2, 4, 4, 5, 10000, 'r-')
+
+%%% plot fitted corr function by nonGau-need
+B = 2;
+j_min = 2;
+j_max = 4;
+
+nu = 4;
+sigma_j = sqrt([1 0.01 0.0002]);
+
+plot_corr_fun_flex(2, sigma_j, 2, 4, nu, 1000, 'r');
+
+%%% plot fitted corr function by Gau-need
+load('beta_hat_needlet.mat')
+sigma_j = sqrt([1 beta_hat(7:8)]);
+plot_corr_fun_flex(2, sigma_j, 2, 4, nu, 1000, 'g');
+
+%%% plot fitted corr function by Matern
+load('beta_hat.mat')
+m = 6;
+n_r = 1e3;
+r_vec = linspace(0, 2, n_r);
+corr_vec = zeros(n_r, 1);
+nu = beta_hat(m+1);
+a = beta_hat(m+2);
+for i = 1:n_r
+    corr_vec(i) = Matern(r_vec(i), nu, a);
+end
+% convert chordal distance to great-circle distance
+r_vec = asin(r_vec/2)*2;
+% plot correlation function
+plot(r_vec, corr_vec, 'c', 'LineWidth', 2)
+
+lg = legend('Empirical corr function', 'Est. corr function (nonGau-need)',...
+    'Est. corr function (Gau-need)', 'Est. corr function (Gau-Matern)');
+set(lg, 'FontSize', 12)
+set(gca, 'FontSize', 12)
+xlabel('Great-circle distance (rad)')
+ylabel('Correlation')
 axis tight
