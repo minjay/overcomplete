7cad4f31f293beb14ee44b548862fcc1de43cd0f
minjay <mjfan@ucar.edu>
2016-06-14 15:32:53 -0600

    adapt to simple case

--- sim/plot_post_sample_simple.m
+++ sim/plot_post_sample_simple.m
@@ -1,14 +1,14 @@
-load('post_samples_rand_sim.mat')
+load('post_samples_simple.mat')
 
 figure
-for i = 1:5
+for i = 1:4
     subplot(3, 2, i)
     plot(post_samples.eta(i, :))
     title(['eta', num2str(i)])
     axis tight
 end
 
-subplot(3, 2, 6)
+subplot(3, 2, 5)
 plot(post_samples.sigma_j_sq(2, :))
 title('sigma2sq')
 axis tight
@@ -16,12 +16,12 @@ axis tight
 theta = 0:0.01:pi;
 
 % non-stationary variance function
-knots = [0 0 0 0 0.5 1 1 1 1]*pi;
+knots = [0 0 0 0 1 1 1 1]*pi;
 [b_mat, ~] = bspline_basismatrix(4, knots, theta);
 
 b_mat(:, 1) = 1;
 
-std_vec = exp(b_mat*eta);
+std_vec = exp(-(theta-pi/2).^2/2/(pi/4)^2);
 
 figure
 plot(theta, std_vec, 'b', 'LineWidth', 2)
