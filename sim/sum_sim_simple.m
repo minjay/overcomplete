b2c37a6eda28d5b8b75fe873591ba3b909e82880
minjay <mjfan@ucar.edu>
2016-06-25 23:24:12 -0700

    adapt to simple case

--- sim/sum_sim_simple.m
+++ sim/sum_sim_simple.m
@@ -3,7 +3,7 @@ eta_est_all = [];
 sigma_j_est_all = [];
 tau_est_all = [];
 for i = 1:10
-    load(['sim_rep', num2str(i), '.mat'])
+    load(['sim_rep_simple', num2str(i), '.mat'])
     eta_est_all = [eta_est_all eta_est];
     sigma_j_est_all = [sigma_j_est_all sigma_j_est(2, :)];
     tau_est_all = [tau_est_all tau_est];
@@ -12,17 +12,19 @@ end
 theta = 0:0.01:pi;
 
 % non-stationary variance function
-knots = [0 0 0 0 0.5 1 1 1 1]*pi;
+knots = [0 0 0 0 1 1 1 1]*pi;
 [b_mat, ~] = bspline_basismatrix(4, knots, theta);
 
 b_mat(:, 1) = 1;
 
+std_vec = exp(-(theta-pi/2).^2/2/(pi/4)^2);
+
+eta_hat = (b_mat'*b_mat)\(b_mat'*log(std_vec)');
+
 subplot('position', [0.1 0.575 0.6 0.375])
-boxplot(eta_est_all', 'Labels', {'0', '1', '2', '3', '4'})
-yl = ylim;
-ylim([min(eta)-0.1 yl(2)])
-for i = 1:5
-    line([i-0.4 i+0.4], [eta(i) eta(i)], 'LineWidth', 2)
+boxplot(eta_est_all', 'Labels', {'0', '1', '2', '3'})
+for i = 1:4
+    line([i-0.4 i+0.4], [eta_hat(i) eta_hat(i)], 'LineWidth', 2)
 end
 title('\eta')
 set(gca, 'FontSize', 12)
@@ -30,7 +32,7 @@ set(gca, 'FontSize', 12)
 subplot('position', [0.1 0.1 0.6 0.375])
 plot(theta, exp(b_mat*eta_est_all))
 hold on
-plot(theta, exp(b_mat*eta), 'k', 'LineWidth', 2)
+plot(theta, std_vec, 'k', 'LineWidth', 2)
 axis tight
 xlabel('Co-latitude')
 ylabel('Standard deviation')
