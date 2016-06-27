be58ec230d53731059e84d3e19c736ca03e4fa8c
minjay <mjfan@ucar.edu>
2016-06-26 22:40:10 -0700

    make it more flexible

--- sim_pred/rand_sampler.m
+++ sim_pred/rand_sampler.m
@@ -1,9 +1,12 @@
-function index = rand_sampler(theta_vec, phi_vec)
+function index = rand_sampler(theta_vec, phi_vec, seed, width)
 
-index = find((phi_vec<=pi-pi/18 | phi_vec>=pi+pi/18) & theta_vec>=pi/6 & theta_vec<=5*pi/6);
+rng(seed)
+lb = rand*(2*pi-width);
+rb = lb+width;
+index = find((phi_vec<=lb | phi_vec>=rb) & theta_vec>=pi/6 & theta_vec<=5*pi/6);
 n = 1e3;
 index1 = randsample(index, n*0.9);
-index = find((phi_vec<=pi-pi/18 | phi_vec>=pi+pi/18) & (theta_vec<pi/6 | theta_vec>5*pi/6));
+index = find((phi_vec<=lb | phi_vec>=rb) & (theta_vec<pi/6 | theta_vec>5*pi/6));
 index2 = randsample(index, n*0.1);
 index = [index1; index2];
 
