867a94e3adbbbd52f7265b8ccb3d4612bd2f094e
minjay <mjfan@ucar.edu>
2016-07-14 21:02:07 -0700

    adapt to needlet plot

--- sim_pred/boxplot_needlet.m
+++ sim_pred/boxplot_needlet.m
@@ -1,20 +1,20 @@
-load('sim_pred_err_Matern_rep_2dot5.mat')
-MSPE_out_2dot5 = MSPE_Matern_out_region_all;
-MSPE_2dot5 = MSPE_Matern_region_all;
-MAE_out_2dot5 = MAE_Matern_out_region_all;
-MAE_2dot5 = MAE_Matern_region_all;
+load('sim_pred_err_rep_2dot5.mat')
+MSPE_out_2dot5 = MSPE_needlet_out_region_all;
+MSPE_2dot5 = MSPE_needlet_region_all;
+MAE_out_2dot5 = MAE_needlet_out_region_all;
+MAE_2dot5 = MAE_needlet_region_all;
 
-load('sim_pred_err_Matern_rep_3.mat')
-MSPE_out_3 = MSPE_Matern_out_region_all;
-MSPE_3 = MSPE_Matern_region_all;
-MAE_out_3 = MAE_Matern_out_region_all;
-MAE_3 = MAE_Matern_region_all;
+load('sim_pred_err_rep_3.mat')
+MSPE_out_3 = MSPE_needlet_out_region_all;
+MSPE_3 = MSPE_needlet_region_all;
+MAE_out_3 = MAE_needlet_out_region_all;
+MAE_3 = MAE_needlet_region_all;
 
-load('sim_pred_err_Matern_rep_4.mat')
-MSPE_out_4 = MSPE_Matern_out_region_all;
-MSPE_4 = MSPE_Matern_region_all;
-MAE_out_4 = MAE_Matern_out_region_all;
-MAE_4 = MAE_Matern_region_all;
+load('sim_pred_err_rep_4.mat')
+MSPE_out_4 = MSPE_needlet_out_region_all;
+MSPE_4 = MSPE_needlet_region_all;
+MAE_out_4 = MAE_needlet_out_region_all;
+MAE_4 = MAE_needlet_region_all;
 
 subplot(2, 2, 1)
 boxplot([MSPE_out_2dot5 MSPE_out_3 MSPE_out_4], 'Labels', {'nu=2.5', 'nu=3', 'nu=4'})
