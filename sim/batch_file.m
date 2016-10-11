fab3fd1e381a0fefd6fe398a3b9c2bc64a16cd7d
minjay <mjfan@ucar.edu>
2016-10-11 09:11:50 -0700

    Add batch file for sim_AM_sigma_rep2.m

---
+++ sim/batch_file.m
@@ -0,0 +1,15 @@
+% batch file for sim_AM_sigma_rep2.m
+
+addpath(genpath('/home/minjay/NeedMat'))
+addpath(genpath('/home/minjay/overcomplete'))
+addpath(genpath('/home/minjay/div_curl'))
+addpath(genpath('/home/minjay/model_output'))
+addpath(genpath('/home/minjay/nonsta_matern'))
+addpath(genpath('/home/minjay/bspline'))
+
+parpool(10)
+
+parfor seed = 1:10
+    sim_AM_sigma_rep2(seed);
+end
+
