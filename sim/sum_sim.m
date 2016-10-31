82d8f9b7e6855dbebe72d2f17dc265feb0a78908
minjay <mjfan@ucar.edu>
2016-10-30 21:57:46 -0700

    Draw median, 5% and 95% quantiles.

--- sim/sum_sim.m
+++ sim/sum_sim.m
@@ -1,3 +1,5 @@
+clear
+
 % summarize sim results
 eta_est_all = [];
 sigma_j_est_all = [];
@@ -17,20 +19,21 @@ knots = [0 0 0 0 0.5 1 1 1 1]*pi;
 
 b_mat(:, 1) = 1;
 
-n_line = size(eta_est_all, 2);
-color_mat = colormap(jet(n_line));
 subplot('position', [0.1 0.1 0.7 0.85])
 fitted = exp(b_mat*eta_est_all);
-hold on
-for i = 1:n_line
-    h = plot(theta, fitted(:, i));
-    % 0.3 represents the transparency
-    h.Color = [color_mat(i, :) 0.5];
-end
+med = median(fitted, 2);
+lb = quantile(fitted, 0.05, 2);
+ub = quantile(fitted, 0.95, 2);
 plot(theta, exp(b_mat*eta), 'k', 'LineWidth', 2)
+hold on
+plot(theta, med, 'b--', 'LineWidth', 2)
+plot(theta, lb, 'r-.', 'LineWidth', 2)
+plot(theta, ub, 'r-.', 'LineWidth', 2)
 axis tight
 xlabel('Co-latitude (rad)')
 ylabel('Standard deviation')
+legend('True', 'Pointwise median', '5% pointwise quantile',...
+    '95% pointwise quantile', 'Location', 'Best')
 set(gca, 'FontSize', 12)
 
 subplot('position', [0.875 0.575 0.1 0.375])
