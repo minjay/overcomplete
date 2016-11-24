2c30eea4f73d5ff794fe545401ed4971a3ff2433
minjay <mjfan@ucar.edu>
2016-11-24 14:35:54 -0800

    Change lambda to 0.01

--- sim/sim_AM_sigma.m
+++ sim/sim_AM_sigma.m
@@ -98,7 +98,7 @@ tau_sq_inv_init = 1/tau_init^2;
 % tuning parameters
 mu_init = zeros(r+1, 1);
 Sigma_init = eye(r+1);
-lambda = 0.05;
+lambda = 0.01;
 % the number of MCMC iterations
 T = 3e5;
 % the length of the burn-in period
