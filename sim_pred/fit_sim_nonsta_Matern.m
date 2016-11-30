c07106f28824aa3fa22cc0e15a46e4ec1336095a
minjay <mjfan@ucar.edu>
2016-11-30 08:52:20 -0800

    Update

--- sim_pred/fit_sim_nonsta_Matern.m
+++ sim_pred/fit_sim_nonsta_Matern.m
@@ -5,9 +5,9 @@ load(['data_sim_', name, '.mat'])
 rng(seed)
 
 % sampling
-[index, index_region] = rand_sampler(theta_vec, phi_vec, width);
-theta_samples = theta_vec(index);
-phi_samples = phi_vec(index);
+[index, index_region] = rand_sampler(phi, width);
+theta_samples = theta(index);
+phi_samples = phi(index);
 Y = Y(index);
 
 [x, y, z] = trans_coord(theta_samples, phi_samples);
@@ -16,18 +16,18 @@ Y = Y(index);
 r = get_chordal_dist(x, y, z);
 
 % non-stationary variance function
-knots = [0 0 0 0 1 1 1 1]*pi;
+knots = [0 0 0 0 0.5 1 1 1 1]*pi;
 [b_mat, ~] = bspline_basismatrix(4, knots, theta_samples);
 
 b_mat(:, 1) = 1;
 
-m = size(b_mat, 2)-1;
+r = size(b_mat, 2)-1;
 
-beta_init = [zeros(1, m+1) 2 10 0.01];
+beta_init = [zeros(1, r+1) 2 10 1e-2];
 negloglik1 = @(beta_all) negloglik_nonsta_Matern(beta_all, r, b_mat, Y);
 
-lb = [-10*ones(1, m+1) 0 0 1e-3];
-ub = [10*ones(1, m+1) 10 Inf Inf];
+lb = [-10*ones(1, r+1) 0 0 1e-3];
+ub = [10*ones(1, r+1) 10 Inf Inf];
 
 [beta_hat, f_min] = nonsta_Matern_fit(negloglik1, beta_init, lb, ub, true);
 
