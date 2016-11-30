804dc502406a61d677f73caf289c7ee14f0e0776
minjay <mjfan@ucar.edu>
2016-11-30 15:08:35 -0800

    Add comments

--- sim_pred/rand_sampler.m
+++ sim_pred/rand_sampler.m
@@ -1,4 +1,7 @@
 function [index, index_region] = rand_sampler(phi, width)
+% randomly split data into training and test data
+% var index gives the index of training data
+% var index_region gives the index of test data
 
 lb = pi-width/2;
 rb = pi+width/2;
