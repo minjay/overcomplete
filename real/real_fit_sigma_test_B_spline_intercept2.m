c1e14e3479dc2da57b95778b50d0afedbe03324b
minjay <mjfan@ucar.edu>
2016-12-01 11:21:42 -0800

    Change save file name

--- real/real_fit_sigma_test_B_spline_intercept2.m
+++ real/real_fit_sigma_test_B_spline_intercept2.m
@@ -100,4 +100,4 @@ options = struct('T', T, 'burn_in', burn_in, 'thin', thin, 'n_report', n_report)
 
 post_samples = Gibbs_sampler_AM_rep_inter(model, data, params, tuning, options);
 
-save('post_samples_real.mat', 'post_samples', 'Npix', 'index', 'theta_samples', 'phi_samples', 'beta_hat')
+save('post_samples_real_noninfo.mat', 'post_samples', 'Npix', 'index', 'theta_samples', 'phi_samples', 'beta_hat')
