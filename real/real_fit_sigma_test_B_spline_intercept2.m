8e29ae0728fff0e50940b140e3180ab93c1b0419
minjay <mjfan@ucar.edu>
2016-12-05 10:51:32 -0800

    Run a little bit more steps

--- real/real_fit_sigma_test_B_spline_intercept2.m
+++ real/real_fit_sigma_test_B_spline_intercept2.m
@@ -80,7 +80,7 @@ mu_init = zeros(r+1, 1);
 Sigma_init = eye(r+1);
 lambda = 0.001;
 % the number of MCMC iterations
-T = 6e5;
+T = 6e5+1e4;
 % the length of the burn-in period
 burn_in = 0;
 % the length of the thinning interval
