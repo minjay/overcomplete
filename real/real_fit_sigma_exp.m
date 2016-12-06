b2caf24a8a985443a4046eea48e4282cb675c49d
minjay <mjfan@ucar.edu>
2016-12-05 22:45:11 -0800

    Add MCMC part.

--- real/real_fit_sigma_exp.m
+++ real/real_fit_sigma_exp.m
@@ -53,4 +53,51 @@ ub = [10*ones(1, r+1) 1 1 Inf];
 
 [beta_hat, f_min] = Gaussian_needlet_fit(negloglik1, beta_init, lb, ub, true);
 
-save('post_samples_real_exp.mat', 'beta_hat')
+delete(gcp)
+
+TT = size(Y, 2);
+
+% init
+% c
+c_init = zeros(M, TT);
+% V
+V_inv_init = ones(M, 1); 
+% sigma_j_sq
+sigma_j_sq_init = beta_hat(end-2:end-1)';
+% eta
+eta_init = beta_hat(1:r+1)';
+% pri_sig of eta_0
+tau_sigma_sq = 1e2;
+% pri_sig of eta
+tau_eta_sq = 1e2;
+% tau
+tau_init = beta_hat(end);
+tau_sq_inv_init = 1/tau_init^2;
+% tuning parameters
+mu_init = zeros(r+1, 1);
+Sigma_init = eye(r+1);
+lambda = 0.001;
+% the number of MCMC iterations
+T = 6e5+1e4;
+% the length of the burn-in period
+burn_in = 0;
+% the length of the thinning interval
+thin = 200;
+% the length of the interval to report progress
+n_report = 100;
+
+model = struct('A', A, 'b_mat', b_mat, 'nu', nu);
+
+data = struct('Y', Y, 'Npix', Npix);
+
+params = struct('c', c_init, 'V', V_inv_init, 'sigma_j_sq', sigma_j_sq_init,...
+    'eta', eta_init, 'tau_sigma_sq', tau_sigma_sq, 'tau_eta_sq', tau_eta_sq,...
+    'tau', tau_sq_inv_init);
+
+tuning = struct('mu', mu_init, 'Sigma', Sigma_init, 'lambda', lambda);
+
+options = struct('T', T, 'burn_in', burn_in, 'thin', thin, 'n_report', n_report, 'save', true);
+
+post_samples = Gibbs_sampler_AM_rep_inter(model, data, params, tuning, options);
+
+save('post_samples_real_exp.mat', 'post_samples', 'Npix', 'index', 'theta_samples', 'phi_samples', 'beta_hat')
