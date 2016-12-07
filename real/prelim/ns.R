b2f315f3496bb94adcc982c810f906c12c35b77e
minjay <mjfan@ucar.edu>
2016-12-06 17:11:11 -0800

    Add comments.

--- real/prelim/ns.R
+++ real/prelim/ns.R
@@ -9,6 +9,8 @@ setwd('/Users/minjay/Documents/MATLAB/Needlets/overcomplete/real/prelim/')
 data = readMat("theta_phi_R.mat")
 
 xs = data$theta[1, ]*4
+# intercept is not needed since it will be added explicitly
+# out of 120/180*pi, the spline becomes linear
 b_mat = ns(x=xs, knots=c(40/180*pi, 80/180*pi), intercept=FALSE, Boundary.knots=c(0, 120/180*pi))
 
 matplot(xs, b_mat, type='l')
