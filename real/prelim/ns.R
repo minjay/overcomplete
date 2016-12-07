f6d4daa8fdc573e0eed53f01abd95f01e0092e95
minjay <mjfan@ucar.edu>
2016-12-06 17:07:21 -0800

    Add a R script to get the natural cubic B-spline matrix

---
+++ real/prelim/ns.R
@@ -0,0 +1,16 @@
+# this script obtains the natural cubic B-spline matrix evaluated at theta's
+rm(list=ls())
+
+require(splines)
+
+library(R.matlab)
+
+setwd('/Users/minjay/Documents/MATLAB/Needlets/overcomplete/real/prelim/')
+data = readMat("theta_phi_R.mat")
+
+xs = data$theta[1, ]*4
+b_mat = ns(x=xs, knots=c(40/180*pi, 80/180*pi), intercept=FALSE, Boundary.knots=c(0, 120/180*pi))
+
+matplot(xs, b_mat, type='l')
+
+writeMat('ns.mat', b_mat=b_mat)
