d8e8cbcfe105e8d8882dde43a0a84e8d5770ea6e
minjay <mjfan@ucar.edu>
2016-12-06 23:24:56 -0800

    Use larger init lambda

---
+++ real/real_fit_sigma_exp3.m
@@ -0,0 +1,97 @@
+addpath(genpath('/home/minjay/NeedMat'))
+addpath(genpath('/home/minjay/overcomplete'))
+addpath(genpath('/home/minjay/div_curl'))
+addpath(genpath('/home/minjay/model_output'))
+addpath(genpath('/home/minjay/nonsta_matern'))
+addpath(genpath('/home/minjay/bspline'))
+
+load('data_EOF_regr_new.mat')
+% work on the data at the first time point
+resid = resid_all(1, :);
+
+rng(1)
+
+% sampling
+n = 4000;
+theta_vec = theta(:);
+phi_vec = phi(:);
+w = sin(theta_vec*4);
+[pot_samples, index] = datasample(resid', n, 'Replace', false,...
+    'Weights', w);
+theta_samples = theta_vec(index);
+phi_samples = phi_vec(index);
+
+% plot
+% plot_samples(theta_vec, index, phi_samples, pot_samples)
+
+% fit
+% parameter specification
+B = 2;
+j_min = 2;
+j_max = 4;
+nu = 4;
+
+% design matrix A
+[Npix, ~, A] = get_A_ss(B, j_min, j_max, theta_samples*4, phi_samples);
+M = size(A, 2);
+
+% non-stationary variance function
+% the first column is all ones
+load('ns.mat')
+b_mat_full = kron(b_mat, ones(size(theta, 1), 1));
+b_mat = b_mat_full(index, :);
+b_mat = [ones(n, 1) b_mat];
+
+r = size(b_mat, 2)-1;
+
+% rescale the observations
+Y = pot_samples/1e3;
+
+load('post_samples_exp2.mat')
+
+TT = size(Y, 2);
+
+% init
+% c
+c_init = zeros(M, TT);
+% V
+V_inv_init = ones(M, 1); 
+% sigma_j_sq
+sigma_j_sq_init = beta_hat(end-2:end-1)';
+% eta
+eta_init = beta_hat(1:r+1)';
+% pri_sig of eta_0
+tau_sigma_sq = 1e2;
+% pri_sig of eta
+tau_eta_sq = 1e2;
+% tau
+tau_init = beta_hat(end);
+tau_sq_inv_init = 1/tau_init^2;
+% tuning parameters
+mu_init = zeros(r+1, 1);
+Sigma_init = eye(r+1);
+lambda = 0.005;
+% the number of MCMC iterations
+T = 6e5+1e4;
+% the length of the burn-in period
+burn_in = 0;
+% the length of the thinning interval
+thin = 200;
+% the length of the interval to report progress
+n_report = 100;
+
+model = struct('A', A, 'b_mat', b_mat, 'nu', nu);
+
+data = struct('Y', Y, 'Npix', Npix);
+
+params = struct('c', c_init, 'V', V_inv_init, 'sigma_j_sq', sigma_j_sq_init,...
+    'eta', eta_init, 'tau_sigma_sq', tau_sigma_sq, 'tau_eta_sq', tau_eta_sq,...
+    'tau', tau_sq_inv_init);
+
+tuning = struct('mu', mu_init, 'Sigma', Sigma_init, 'lambda', lambda);
+
+options = struct('T', T, 'burn_in', burn_in, 'thin', thin, 'n_report', n_report, 'save', true);
+
+post_samples = Gibbs_sampler_AM_rep_inter(model, data, params, tuning, options);
+
+save('post_samples_real_exp3.mat', 'post_samples', 'Npix', 'index', 'theta_samples', 'phi_samples', 'beta_hat')
