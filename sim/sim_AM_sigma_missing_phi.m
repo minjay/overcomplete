e722cef1a325ccf8d454144b950771b7c913434b
minjay <mjfan@ucar.edu>
2016-12-16 15:43:52 -0800

    Add a copy.

---
+++ sim/sim_AM_sigma_missing_phi.m
@@ -0,0 +1,130 @@
+addpath(genpath('/home/minjay/NeedMat'))
+addpath(genpath('/home/minjay/overcomplete'))
+addpath(genpath('/home/minjay/div_curl'))
+addpath(genpath('/home/minjay/model_output'))
+addpath(genpath('/home/minjay/nonsta_matern'))
+addpath(genpath('/home/minjay/bspline'))
+
+clear
+rng(1)
+
+nu = 4;
+alpha = 3;
+
+tau = 0.1;
+
+% the grid
+B = 2;
+Nside = 8;
+tp = pix2ang(Nside, 'nest', false);
+N = length(tp);
+theta = zeros(N, 1);
+phi = zeros(N, 1);
+for i = 1:N
+    theta(i) = tp{i}(1);
+    phi(i) = tp{i}(2);
+end
+
+% perturbation
+theta = theta+randn(N, 1)*pi/10;
+theta(theta<0) = theta(theta<0)+pi;
+theta(theta>pi) = theta(theta>pi)-pi;
+phi = phi+randn(N, 1)*2*pi/10;
+phi(phi<0) = phi(phi<0)+2*pi;
+phi(phi>2*pi) = phi(phi>2*pi)-2*pi;
+
+index = find(phi<=3/2*pi);
+theta = theta(index);
+phi = phi(index);
+N = length(index);
+
+j_min = 2;
+j_max = 3;
+
+% design matrix A
+[Npix, ~, A] = get_A_ss(B, j_min, j_max, theta, phi);
+M = size(A, 2); 
+
+sigma_j = B.^(-alpha/2*(j_min:j_max));
+sigma_j = sigma_j/sigma_j(1);
+
+% non-stationary variance function
+knots = [0 0 0 0 0.5 1 1 1 1]*pi;
+[b_mat, ~] = bspline_basismatrix(4, knots, theta);
+
+b_mat(:, 1) = 1;
+
+r = size(b_mat, 2)-1;
+
+rng(2)
+eta = randn(r+1, 1);
+std_vec = exp(b_mat*eta);
+DA = zeros(N, M);
+for i = 1:N
+    DA(i, :) = std_vec(i)*A(i, :);
+end
+
+c = zeros(M, 1);
+st = 1;
+for j = j_min:j_max
+    index_j = j-j_min+1;
+    range = st:st+Npix(index_j)-1;
+    c(range) = sigma_j(index_j)*trnd(nu, Npix(index_j), 1);
+    st = st+Npix(index_j);
+end
+
+Y = DA*c+randn(N, 1)*tau;
+
+% get init values for MCMC
+beta_init = [zeros(1, r+1) 0.1^2 1e-2];
+negloglik1 = @(beta_all) negloglik_Gaussian_needlet(beta_all, b_mat, Y, Npix, A);
+
+lb = [-10*ones(1, r+1) 0 1e-3];
+ub = [10*ones(1, r+1) 1 Inf];
+
+[beta_hat, f_min] = Gaussian_needlet_fit(negloglik1, beta_init, lb, ub, true);
+
+% init
+% c
+c_init = zeros(M, 1);
+% V
+V_inv_init = ones(M, 1); 
+% sigma_j_sq
+sigma_j_sq_init = beta_hat(end-1);
+% eta
+eta_init = beta_hat(1:r+1)';
+% pri_sig of eta_0
+tau_sigma_sq = 1e2;
+% pri_sig of eta
+tau_eta_sq = 1e2;
+% tau
+tau_init = beta_hat(end);
+tau_sq_inv_init = 1/tau_init^2;
+% tuning parameters
+mu_init = zeros(r+1, 1);
+Sigma_init = eye(r+1);
+lambda = 0.05;
+% the number of MCMC iterations
+T = 3e5;
+% the length of the burn-in period
+burn_in = 0;
+% the length of the thinning interval
+thin = 200;
+% the length of the interval to report progress
+n_report = 100;
+
+model = struct('A', A, 'b_mat', b_mat, 'nu', nu);
+
+data = struct('Y', Y, 'Npix', Npix);
+
+params = struct('c', c_init, 'V', V_inv_init, 'sigma_j_sq', sigma_j_sq_init,...
+    'eta', eta_init, 'tau_sigma_sq', tau_sigma_sq, 'tau_eta_sq', tau_eta_sq,...
+    'tau', tau_sq_inv_init);
+
+tuning = struct('mu', mu_init, 'Sigma', Sigma_init, 'lambda', lambda);
+
+options = struct('T', T, 'burn_in', burn_in, 'thin', thin, 'n_report', n_report, 'save', false);
+
+post_samples = Gibbs_sampler_AM_rep_inter(model, data, params, tuning, options);
+
+save('post_samples_missing_phi.mat', 'post_samples', 'sigma_j', 'eta', 'c', 'tau', 'theta', 'phi')
