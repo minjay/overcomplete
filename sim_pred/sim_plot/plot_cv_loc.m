5102a1c8e48a70a643f0bf31c148ff75c42496dc
minjay <mjfan@ucar.edu>
2016-12-22 18:13:04 -0800

    Plot on the HEALPix grid.

--- sim_pred/sim_plot/plot_cv_loc.m
+++ sim_pred/sim_plot/plot_cv_loc.m
@@ -3,28 +3,40 @@ clear
 name = '2dot5';
 
 load(['data_sim_', name, '.mat'])
+load(['post_samples_', name, '_1', '.mat'])
 
-seed = 1;
+% convert theta and phi
+theta = pi/2-theta;
+% move the region to the center
+phi = phi+pi;
+% make sure phi is in [0, 2*pi)
+phi(phi>=2*pi) = phi(phi>=2*pi)-2*pi;
+phi(phi>pi) = phi(phi>pi)-2*pi;
 
-rng(seed)
+[HX, HY] = sph2hammer(phi, theta);
 
-width = pi/9;
-
-% sampling
-[index, index_region] = rand_sampler(theta_vec, phi_vec, width);
-
-N = length(theta_vec);
+N = length(theta);
 index_pred = setdiff(1:N, index);
 index_pred_out_region = setdiff(index_pred, index_region);
 index_pred_region = index_region;
 
-scatter(phi_vec(index), theta_vec(index), 100, 'b.')
+scatter(HX(index), HY(index), 20, 'bo')
 hold on
-scatter(phi_vec(index_pred_out_region), theta_vec(index_pred_out_region), 100, 'g.')
-scatter(phi_vec(index_pred_region), theta_vec(index_pred_region), 100, 'r.')
-axis tight
-axis equal
+scatter(HX(index_pred_out_region), HY(index_pred_out_region), 30, 'm+')
+scatter(HX(index_pred_region), HY(index_pred_region), 30, 'r*')
 
-xlabel('Longitude (rad)')
-ylabel('Co-latitude (rad)')
+% draw contour
+th = linspace(-pi/2,pi/2,101);
+lam = -pi+0*th;
+[xh,yh] = sph2hammer(lam,th);
+plot(xh,yh,'k');
+lam = pi+0*th;
+[xh,yh] = sph2hammer(lam,th);
+plot(xh,yh,'k');
+
+axis tight
+axis off
+
+legend('Observed locations', 'Short-range predicted locations', 'Long-range predicted locations',...
+    'Location', 'SouthOutside')
 set(gca, 'FontSize', 12)
