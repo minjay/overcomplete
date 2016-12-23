bd8673a45ec18c39fd84f2cc50119ed908e3871f
minjay <mjfan@ucar.edu>
2016-12-22 16:31:00 -0800

    Output len and cp.

--- sim_pred/pred_err_nonGau_needlet.m
+++ sim_pred/pred_err_nonGau_needlet.m
@@ -10,11 +10,21 @@ MAE_out_all = zeros(T, 1);
 MAE_in_all = zeros(T, 1);
 CRPS_out_all = zeros(T, 1);
 CRPS_in_all = zeros(T, 1);
+len_90_out_all = zeros(T, 1);
+len_90_in_all = zeros(T, 1);
+len_50_out_all = zeros(T, 1);
+len_50_in_all = zeros(T, 1);
+cp_90_out_all = zeros(T, 1);
+cp_90_in_all = zeros(T, 1);
+cp_50_out_all = zeros(T, 1);
+cp_50_in_all = zeros(T, 1);
 
 for t = 1:T
     t
     load(['post_samples_', name, '_', num2str(t), '.mat'])
-    [MSPE_out, MSPE_in, MAE_out, MAE_in, CRPS_out, CRPS_in] = ...
+    [MSPE_out, MSPE_in, MAE_out, MAE_in, CRPS_out, CRPS_in,...   
+        avg_len_90_out, avg_len_90_in, avg_len_50_out, avg_len_50_in,...
+        cp_90_out, cp_90_in, cp_50_out, cp_50_in] = ...
         pred_sim_nonGau_needlet(name, post_samples, index, index_region);
     MSPE_out_all(t) = MSPE_out;
     MSPE_in_all(t) = MSPE_in;
@@ -22,9 +32,19 @@ for t = 1:T
     MAE_in_all(t) = MAE_in;
     CRPS_out_all(t) = CRPS_out;
     CRPS_in_all(t) = CRPS_in;
+    len_90_out_all(t) = avg_len_90_out;
+    len_90_in_all(t) = avg_len_90_in;
+    len_50_out_all(t) = avg_len_50_out;
+    len_50_in_all(t) = avg_len_50_in;
+    cp_90_out_all(t) = cp_90_out;
+    cp_90_in_all(t) = cp_90_in;
+    cp_50_out_all(t) = cp_50_out;
+    cp_50_in_all(t) = cp_50_in;
 end
 
 save(['sim_pred_err_nonGau_needlet_', name, '.mat'], 'MSPE_out_all', 'MSPE_in_all',...
-    'MAE_out_all', 'MAE_in_all', 'CRPS_out_all', 'CRPS_in_all')
+    'MAE_out_all', 'MAE_in_all', 'CRPS_out_all', 'CRPS_in_all',...
+    'len_90_out_all', 'len_90_in_all', 'len_50_out_all', 'len_50_in_all',...
+    'cp_90_out_all', 'cp_90_in_all', 'cp_50_out_all', 'cp_50_out_all')
 
 end
