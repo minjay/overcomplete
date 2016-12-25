5a0182106e69794b485dcbcc4d641a9d4df37c8e
minjay <mjfan@ucar.edu>
2016-12-24 20:59:46 -0800

    Add commands to get the derivate of ns.

--- real/prelim/ns.R
+++ real/prelim/ns.R
@@ -4,6 +4,7 @@ rm(list=ls())
 require(splines)
 
 library(R.matlab)
+library(JM)
 
 setwd('/Users/minjay/Documents/MATLAB/Needlets/overcomplete/real/prelim/')
 data = readMat("theta_phi_R.mat")
@@ -13,6 +14,13 @@ xs = data$theta[1, ]*4
 # out of 120/180*pi, the spline becomes linear
 b_mat = ns(x=xs, knots=c(40/180*pi, 80/180*pi), intercept=FALSE, Boundary.knots=c(0, 120/180*pi))
 
+# derivatives of basis functions at the same locations
+b_mat_deriv = dns(x=xs, knots=c(40/180*pi, 80/180*pi), intercept=FALSE, Boundary.knots=c(0, 120/180*pi))
+
 matplot(xs, b_mat, type='l')
 
+matplot(xs, b_mat_deriv, type='l')
+
 writeMat('ns.mat', b_mat=b_mat)
+
+writeMat('ns_deriv.mat', b_mat_deriv=b_mat_deriv)
