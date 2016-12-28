a90f1ab819e633ff31a50273383e9382f84f3a4c
minjay <mjfan@ucar.edu>
2016-12-27 16:15:02 -0800

    Use inner knots 30, 60, 90.

--- real/prelim/ns.R
+++ real/prelim/ns.R
@@ -14,6 +14,8 @@ xs = data$theta[1, ]*4
 # out of 120/180*pi, the spline becomes linear
 b_mat = ns(x=xs, knots=c(40/180*pi, 80/180*pi), intercept=FALSE, Boundary.knots=c(0, 120/180*pi))
 
+b_mat_fine = ns(x=xs, knots=c(30/180*pi, 60/180*pi, 90/180*pi), intercept=FALSE, Boundary.knots=c(0, 120/180*pi))
+
 # derivatives of basis functions at the same locations
 b_mat_deriv = dns(x=xs, knots=c(40/180*pi, 80/180*pi), intercept=FALSE, Boundary.knots=c(0, 120/180*pi))
 
@@ -21,6 +23,6 @@ matplot(xs, b_mat, type='l')
 
 matplot(xs, b_mat_deriv, type='l')
 
-writeMat('ns.mat', b_mat=b_mat)
+writeMat('ns.mat', b_mat=b_mat, b_mat_fine=b_mat_fine)
 
 writeMat('ns_deriv.mat', b_mat_deriv=b_mat_deriv)
