82dad8d088a75c653fd2d8bb68e39f38593066d6
minjay <mjfan@ucar.edu>
2017-05-30 11:15:26 -0700

    Add quantile scores.

--- sim_pred/pred_err_nonGau_needlet.m
+++ sim_pred/pred_err_nonGau_needlet.m
@@ -18,13 +18,18 @@ cp_90_out_all = zeros(T, 1);
 cp_90_in_all = zeros(T, 1);
 cp_50_out_all = zeros(T, 1);
 cp_50_in_all = zeros(T, 1);
+QS_95_out_all = zeros(T, 1);
+QS_95_in_all = zeros(T, 1);
+QS_05_out_all = zeros(T, 1);
+QS_05_in_all = zeros(T, 1);
 
 for t = 1:T
     t
     load(['post_samples_', name, '_', num2str(t), '.mat'])
     [MSPE_out, MSPE_in, MAE_out, MAE_in, CRPS_out, CRPS_in,...   
         avg_len_90_out, avg_len_90_in, avg_len_50_out, avg_len_50_in,...
-        cp_90_out, cp_90_in, cp_50_out, cp_50_in] = ...
+        cp_90_out, cp_90_in, cp_50_out, cp_50_in, QS_95_out, QS_95_in,...
+        QS_05_out, QS_05_in] = ...
         pred_sim_nonGau_needlet(name, post_samples, index, index_region);
     MSPE_out_all(t) = MSPE_out;
     MSPE_in_all(t) = MSPE_in;
@@ -40,11 +45,16 @@ for t = 1:T
     cp_90_in_all(t) = cp_90_in;
     cp_50_out_all(t) = cp_50_out;
     cp_50_in_all(t) = cp_50_in;
+    QS_95_out_all(t) = QS_95_out;
+    QS_95_in_all(t) = QS_95_in;
+    QS_05_out_all(t) = QS_05_out;
+    QS_05_in_all(t) = QS_05_in;
 end
 
 save(['sim_pred_err_nonGau_needlet_', name, '.mat'], 'MSPE_out_all', 'MSPE_in_all',...
     'MAE_out_all', 'MAE_in_all', 'CRPS_out_all', 'CRPS_in_all',...
     'len_90_out_all', 'len_90_in_all', 'len_50_out_all', 'len_50_in_all',...
-    'cp_90_out_all', 'cp_90_in_all', 'cp_50_out_all', 'cp_50_in_all')
+    'cp_90_out_all', 'cp_90_in_all', 'cp_50_out_all', 'cp_50_in_all',...
+    'QS_95_out_all', 'QS_95_in_all', 'QS_05_out_all', 'QS_05_in_all')
 
 end
