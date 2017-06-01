2186f30ea885808ae881e1002c508d31ffabfee9
minjay <mjfan@ucar.edu>
2017-06-01 00:13:15 -0700

    Increase T to 1e6.

--- real/real_fit_sigma_exp3.m
+++ real/real_fit_sigma_exp3.m
@@ -72,7 +72,7 @@ mu_init = zeros(r+1, 1);
 Sigma_init = eye(r+1);
 lambda = 0.005;
 % the number of MCMC iterations
-T = 6e5+1e4;
+T = 1e6;
 % the length of the burn-in period
 burn_in = 0;
 % the length of the thinning interval
