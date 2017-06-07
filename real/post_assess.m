24ab016a5321f769fd72622fc192521fb6ee76be
minjay <mjfan@ucar.edu>
2017-06-07 15:26:39 -0700

    Use nu=2.5.

--- real/post_assess.m
+++ real/post_assess.m
@@ -3,7 +3,7 @@
 clear
 
 % load fitted result
-load('post_samples_real_exp3.mat')
+load('post_samples_real_exp3_nu2dot5.mat')
 
 % load data
 load('data_EOF_regr_new.mat')
@@ -21,14 +21,14 @@ load('ns.mat')
 b_mat = kron(b_mat, ones(size(theta, 1), 1));
 b_mat = [ones(N, 1) b_mat];
 
-st = 2001;
-en = 3000;
-T = en-st+1;
-Ac = A*post_samples.c(:, st:en);
+st = 3001;
+en = 5000;
+T = (en-st+1)/2;
+Ac = A*post_samples.c(:, st:2:en);
 Y_pred_all = zeros(N, T);
-std_vec_all = exp(b_mat*post_samples.eta(:, st:en));
+std_vec_all = exp(b_mat*post_samples.eta(:, st:2:en));
 rng(1)
-err_all = randn(1, T).*(1./sqrt(post_samples.tau_sq_inv(st:en)));
+err_all = randn(1, T).*(1./sqrt(post_samples.tau_sq_inv(st:2:en)));
 
 for t = 1:T
     Y_pred_all(:, t) = (std_vec_all(:, t).*Ac(:, t)+err_all(t))*1e3;
