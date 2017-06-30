cab49be2cbfa398a71c9bf1739e319c52119033e
minjay <mjfan@ucar.edu>
2017-06-29 17:11:10 -0700

    Use different initial values for Gaussian needlet model.

---
+++ real/real_fit_sigma_exp2_diff_init.m
@@ -0,0 +1,59 @@
+addpath(genpath('/home/minjay/NeedMat'))
+addpath(genpath('/home/minjay/overcomplete'))
+addpath(genpath('/home/minjay/div_curl'))
+addpath(genpath('/home/minjay/model_output'))
+addpath(genpath('/home/minjay/nonsta_matern'))
+addpath(genpath('/home/minjay/bspline'))
+
+load('data_EOF_regr_new.mat')
+% work on the data at the first time point
+resid = resid_all(1, :);
+
+rng(1)
+
+% sampling
+n = 4000;
+theta_vec = theta(:);
+phi_vec = phi(:);
+w = sin(theta_vec*4);
+[pot_samples, index] = datasample(resid', n, 'Replace', false,...
+    'Weights', w);
+theta_samples = theta_vec(index);
+phi_samples = phi_vec(index);
+
+% plot
+% plot_samples(theta_vec, index, phi_samples, pot_samples)
+
+% fit
+% parameter specification
+B = 2;
+j_min = 2;
+j_max = 4;
+nu = 4;
+
+% design matrix A
+[Npix, ~, A] = get_A_ss(B, j_min, j_max, theta_samples*4, phi_samples);
+M = size(A, 2);
+
+% non-stationary variance function
+% the first column is all ones
+load('ns.mat')
+b_mat_full = kron(b_mat, ones(size(theta, 1), 1));
+b_mat = b_mat_full(index, :);
+b_mat = [ones(n, 1) b_mat];
+
+r = size(b_mat, 2)-1;
+
+% rescale the observations
+Y = pot_samples/1e3;
+
+% get init values for MCMC
+beta_init = [zeros(1, r) 0.1^2 0.01^2 0.001^2 1e-2];
+negloglik1 = @(beta_all) negloglik_Gaussian_needlet(beta_all, b_mat, Y, Npix, A);
+
+lb = [-10*ones(1, r) 0 0 0 1e-3];
+ub = [10*ones(1, r) 10 10 10 Inf];
+
+[beta_hat, f_min] = Gaussian_needlet_fit(negloglik1, beta_init, lb, ub, true);
+
+save('post_samples_exp2.mat', 'beta_hat')
