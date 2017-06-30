06c57c8fd7000b8d09ace4b712ad30772689a87b
minjay <mjfan@ucar.edu>
2017-06-30 10:45:00 -0700

    Fix eta0 at 0.

--- sim_pred/fit_sim_needlet.m
+++ sim_pred/fit_sim_needlet.m
@@ -30,11 +30,12 @@ b_mat(:, 1) = 1;
 r = size(b_mat, 2)-1;
 
 % get init values for MCMC
-beta_init = [zeros(1, r+1) 0.1^2 1e-2];
+beta_init = [zeros(1, r) 1 0.1^2 1e-2];
 negloglik1 = @(beta_all) negloglik_Gaussian_needlet(beta_all, b_mat, Y, Npix, A);
 
-lb = [-10*ones(1, r+1) 0 1e-3];
 ub = [10*ones(1, r+1) 1 Inf];
+lb = [-10*ones(1, r) 0 0 1e-3];
+ub = [10*ones(1, r) 10 10 Inf];
 
 [beta_hat, f_min] = Gaussian_needlet_fit(negloglik1, beta_init, lb, ub, false);
 
@@ -44,19 +45,17 @@ c_init = zeros(M, 1);
 % V
 V_inv_init = ones(M, 1); 
 % sigma_j_sq
-sigma_j_sq_init = beta_hat(end-1);
+sigma_j_sq_init = beta_hat(end-2:end-1)';
 % eta
-eta_init = beta_hat(1:r+1)';
-% pri_sig of eta_0
-tau_sigma_sq = 1e2;
+eta_init = [0; beta_hat(1:r)'];
 % pri_sig of eta
 tau_eta_sq = 1e2;
 % tau
 tau_init = beta_hat(end);
 tau_sq_inv_init = 1/tau_init^2;
 % tuning parameters
-mu_init = zeros(r+1, 1);
-Sigma_init = eye(r+1);
+mu_init = zeros(r, 1);
+Sigma_init = eye(r);
 lambda = 0.05;
 % the number of MCMC iterations
 T = 1e6;
@@ -72,14 +71,13 @@ model = struct('A', A, 'b_mat', b_mat, 'nu', nu);
 data = struct('Y', Y, 'Npix', Npix);
 
 params = struct('c', c_init, 'V', V_inv_init, 'sigma_j_sq', sigma_j_sq_init,...
-    'eta', eta_init, 'tau_sigma_sq', tau_sigma_sq, 'tau_eta_sq', tau_eta_sq,...
-    'tau', tau_sq_inv_init);
+    'eta', eta_init, 'tau_eta_sq', tau_eta_sq, 'tau', tau_sq_inv_init);
 
 tuning = struct('mu', mu_init, 'Sigma', Sigma_init, 'lambda', lambda);
 
 options = struct('T', T, 'burn_in', burn_in, 'thin', thin, 'n_report', n_report, 'save', false);
 
-post_samples = Gibbs_sampler_AM_rep_inter(model, data, params, tuning, options);
+post_samples = Gibbs_sampler_AM_reparam(model, data, params, tuning, options);
 
 if flag
     save(['post_samples_', name, '_', num2str(seed), '.mat'], 'post_samples', 'Npix',...
