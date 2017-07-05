0a7c5db3ce1005dbc0c826dad75f214ce489412a
minjay <mjfan@ucar.edu>
2017-07-05 16:00:09 -0700

    Update box plots.

--- sim/sum_sim_2.m
+++ sim/sum_sim_2.m
@@ -2,8 +2,6 @@ clear
 
 load('sim_rep2.mat')
 
-sigma_j_est_all = sigma_j_est_all(2, :);
-
 theta = 0:0.01:pi;
 
 % non-stationary variance function
@@ -12,13 +10,13 @@ knots = [0 0 0 0 0.5 1 1 1 1]*pi;
 
 b_mat(:, 1) = 1;
 
-subplot('position', [0.1 0.575 0.6 0.375])
-bh = boxplot(eta_est_all', 'Labels', {'0', '1', '2', '3', '4'});
+subplot('position', [0.1 0.575 0.5 0.375])
+bh = boxplot(eta_est_all(2:end, :)', 'widths', 0.4, 'Labels', {'1', '2', '3', '4'});
 set(bh(6, :), 'LineWidth', 1.5)
 yl = ylim;
 ylim([min(eta)-0.1 yl(2)])
-for i = 1:5
-    line([i-0.4 i+0.4], [eta(i) eta(i)], 'LineWidth', 1.5, 'LineStyle', '--', 'Color', 'k')
+for i = 1:4
+    line([i-0.4 i+0.4], [eta(i+1) eta(i+1)], 'LineWidth', 1.5, 'LineStyle', '--', 'Color', 'k')
 end
 title('\eta')
 set(gca, 'FontSize', 12)
@@ -40,15 +38,17 @@ legend('Pointwise median', 'True', '5% pointwise quantile',...
     '95% pointwise quantile', 'Location', 'Best')
 set(gca, 'FontSize', 12)
 
-subplot('position', [0.8 0.575 0.15 0.375])
-bh = boxplot(sigma_j_est_all', 'widths', 0.4);
+subplot('position', [0.7 0.575 0.25 0.375])
+bh = boxplot(sigma_j_est_all', 'widths', 0.4, 'Labels', {'2', '3'});
 set(bh(6, :), 'linewidth', 1.5)
-line([1-0.4 1+0.4], [sigma_j(2) sigma_j(2)], 'LineWidth', 1.5, 'LineStyle', '--', 'Color', 'k')
-title('\sigma_3')
+for i = 1:2
+    line([i-0.4 i+0.4], [sigma_j(i) sigma_j(i)], 'LineWidth', 1.5, 'LineStyle', '--', 'Color', 'k')
+end
+title('\sigma')
 set(gca, 'FontSize', 12)
 
 subplot('position', [0.8 0.1 0.15 0.375])
-bh = boxplot(tau_est_all', 'widths', 0.4);
+bh = boxplot(tau_est_all', 'widths', 0.4, 'Labels', {''});
 set(bh(6, :), 'linewidth', 1.5)
 line([1-0.4 1+0.4], [tau tau], 'LineWidth', 1.5, 'LineStyle', '--', 'Color', 'k')
 title('\tau')
